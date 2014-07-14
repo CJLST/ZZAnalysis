b62b70af0defc95bdf96bbcf434edd9924638873
Stefano Casasso <stefano.casasso@cern.ch>
2014-07-14 10:23:57 +0200

    using embedded Z1-Z2 info in the candidate

--- AnalysisStep/interface/Comparators.h
+++ AnalysisStep/interface/Comparators.h
@@ -32,19 +32,12 @@ namespace Comparators {
       pat::CompositeCandidate& cand_i = candCollection[i];
       pat::CompositeCandidate& cand_j = candCollection[j];
 
-      int idxZ1_i = 0; 
-      int idxZ2_i = 1;
-      if(fabs(cand_i.daughter(0)->masterClone().get()->mass()-Comparators::ZmassValue)>=fabs(cand_i.daughter(1)->masterClone().get()->mass()-Comparators::ZmassValue)) swap(idxZ1_i,idxZ2_i);
-      const reco::Candidate* Z1_i = cand_i.daughter(idxZ1_i)->masterClone().get();
-      
-      int idxZ1_j = 0; 
-      int idxZ2_j = 1;
-      if(fabs(cand_j.daughter(0)->masterClone().get()->mass()-Comparators::ZmassValue)>=fabs(cand_j.daughter(1)->masterClone().get()->mass()-Comparators::ZmassValue)) swap(idxZ1_j,idxZ2_j);
-      const reco::Candidate* Z1_j = cand_j.daughter(idxZ1_j)->masterClone().get();
+      const reco::Candidate* Z1_i = cand_i.daughter("Z1")->masterClone().get();      
+      const reco::Candidate* Z1_j = cand_j.daughter("Z1")->masterClone().get();
       
       if ( fabs(Z1_i->mass()-Z1_j->mass()) < 1e-04 ){
-	const reco::Candidate* Z2_i = cand_i.daughter(idxZ2_i)->masterClone().get();
-	const reco::Candidate* Z2_j = cand_j.daughter(idxZ2_j)->masterClone().get();
+	const reco::Candidate* Z2_i = cand_i.daughter("Z2")->masterClone().get();
+	const reco::Candidate* Z2_j = cand_j.daughter("Z2")->masterClone().get();
 	double ptSumZ2_i = Z2_i->daughter(0)->masterClone().get()->pt() + Z2_i->daughter(1)->masterClone().get()->pt();
 	double ptSumZ2_j = Z2_j->daughter(0)->masterClone().get()->pt() + Z2_j->daughter(1)->masterClone().get()->pt();
 	if (ptSumZ2_i > ptSumZ2_j) {
