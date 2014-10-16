7dac52e3a8e8313557333c3c87e586ff440ccee5
Giacomo Ortona <gortona@cern.ch>
2014-10-16 18:38:24 +0200

    Start merging miniAOD and OPWF by deleting/adding files not in common

---
+++ analyzer_7XSync.py
@@ -0,0 +1,98 @@
+### ----------------------------------------------------------------------
+###
+### HCP Synchronization file for 533.
+###
+### FIXME: the path includes the rochester correction (which is a passtrough for 53X right now);
+### the syncronization is supposed to be done with no escale applied
+###
+###----------------------------------------------------------------------
+
+LEPTON_SETUP = 2012
+PD = ""
+MCFILTER = ""
+ELECORRTYPE   = "None" # "None", "Moriond", or "Paper"
+ELEREGRESSION = "None" # "None", "Moriond", "PaperNoComb", or "Paper" 
+APPLYMUCORR = False
+
+#For DATA: 
+#IsMC = False
+#PD = "DoubleEle"
+
+# Get absolute path
+import os
+PyFilePath = os.environ['CMSSW_BASE'] + "/src/ZZAnalysis/AnalysisStep/test/"
+
+### ----------------------------------------------------------------------
+### Standard sequence
+### ----------------------------------------------------------------------
+
+execfile(PyFilePath + "analyzer.py")
+
+
+### ----------------------------------------------------------------------
+  ### Replace parameters
+### ----------------------------------------------------------------------
+process.source.fileNames = cms.untracked.vstring(
+    #'/store/cmst3/user/gpetrucc/miniAOD/v1/GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6_PU_S14_PAT.root'
+    '/store/cmst3/user/gpetrucc/miniAOD/v1/GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6_PU_S14_PAT_big.root' #S14 = 50ns scenario, GT: PLS170_V6AN1
+    
+    #"/store/mc/Spring14miniaod/GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_POSTLS170_V5-v1/00000/0881ABEB-2709-E411-9E42-00145EDD7581.root" # 1st file from the central 20bx25 sample, GT: PLS170_V7AN1
+    )
+
+
+### FIXME: DEBUGGING ONLY, turns off smearing if used with special tag
+#process.calibratedPatElectrons.synchronization = True
+#process.calibratedMuons.fakeSmearing = cms.bool(True)
+
+#process.appendPhotons.debug = cms.untracked.bool(True)
+
+process.maxEvents.input = -1
+
+# Silence output
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 5000
+
+
+### ----------------------------------------------------------------------
+### Analyzer for Plots
+### ----------------------------------------------------------------------
+
+
+#process.source.eventsToProcess = cms.untracked.VEventRange("1:122490", "1:1343", "1:177684")
+#process.source.eventsToProcess = cms.untracked.VEventRange("1:30602")
+#process.source.eventsToProcess = cms.untracked.VEventRange("191062:303:330091192")
+
+# Debug
+process.dumpUserData =  cms.EDAnalyzer("dumpUserData",
+     dumpTrigger = cms.untracked.bool(True),
+     muonSrc = cms.InputTag("slimmedMuons"),
+     electronSrc = cms.InputTag("slimmedElectrons"),
+#     muonSrc = cms.InputTag("appendPhotons:muons"), 
+#     electronSrc = cms.InputTag("appendPhotons:electrons"),
+     candidateSrcs = cms.PSet(
+        Zmm   = cms.InputTag("MMCand"),
+        Zee   = cms.InputTag("EECand"),
+#        Z     = cms.InputTag("ZCand"),                                  
+        MMMM  = cms.InputTag("MMMMCand"),
+        EEEE  = cms.InputTag("EEEECand"),
+        EEMM  = cms.InputTag("EEMMCand"),
+     )
+)
+
+
+#Print MC history
+#process.p = cms.EndPath(process.printTree)
+
+#Dump reconstructed variables
+#process.dump = cms.Path(process.dumpUserData)
+
+#process.Plots4mu.dumpMC   = cms.untracked.bool(True)
+#process.Plots4e.dumpMC    = cms.untracked.bool(True)
+#process.Plots2e2mu.dumpMC = cms.untracked.bool(True)
+
+
+
+# replace the paths in analyzer.py
+process.p = cms.EndPath( process.Plots4mu + process.Plots4e + process.Plots2e2mu )
+process.trees = cms.EndPath(process.ZZ4muTree * process.ZZ4eTree * process.ZZ2e2muTree )
+
---
+++ checkout_70X.csh
@@ -0,0 +1,39 @@
+#!/bin/tcsh -fe
+
+############## For miniAOD/CMSSW_7_0_7 patch1
+
+#ZZAnalysis
+git clone https://github.com/CJLST/ZZAnalysis.git ZZAnalysis
+(cd ZZAnalysis; git checkout giacomo_miniAOD)
+
+#effective areas (to be updated)
+git clone -n https://github.com/latinos/UserCode-sixie-Muon-MuonAnalysisTools Muon/MuonAnalysisTools
+(cd Muon/MuonAnalysisTools ; git checkout master -- interface/MuonEffectiveArea.h )
+
+#Note we did not eventually adopt the latest EA update, we kept V00-00-30-00 of UserCode/EGamma/EGammaAnalysisTools/interface/ElectronEffectiveArea.h .  c0db796 corresponds to it.
+git clone -n https://github.com/cms-analysis/EgammaAnalysis-ElectronTools EGamma/EGammaAnalysisTools
+(cd EGamma/EGammaAnalysisTools; git checkout c0db796 -- interface/ElectronEffectiveArea.h) 
+
+#MuScleFit: probably tbf
+#git clone https://github.com/scasasso/usercode MuScleFit
+
+#MELA
+git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
+(cd ZZMatrixElement ; git checkout -b from-ac1acaa ac1acaa)
+
+#MELA dependencies
+git clone https://github.com/usarica/HiggsAnalysis-CombinedLimit HiggsAnalysis/CombinedLimit
+(cd HiggsAnalysis/CombinedLimit ; git checkout US_slc6root53417)
+
+#photon ISO information for FSR 
+git clone -n https://github.com/VBF-HZZ/UFHZZAnalysisRun2
+(cd UFHZZAnalysisRun2 ; git checkout master FSRPhotons)
+
+#Not needed anymore (dependency removed)
+#git clone https://github.com/HZZ4l/CombinationPy.git HZZ4L_Combination/CombinationPy
+
+# Not needed, for the time being
+#git clone https://github.com/msnowball/HCSaW Higgs/Higgs_CS_and_Width
+#cd Higgs/Higgs_CS_and_Width
+#git filter-branch --subdirectory-filter Higgs_CS_and_Width
+#cd -
