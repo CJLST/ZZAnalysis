a86fcef4408007f198ad5d3ac52d050963105ccc
Hengne <Hengne.Li@CERN.CH>
2016-02-24 12:43:52 +0100

    add also Data JEC

--- AnalysisStep/test/MasterPy/ZZ4lAnalysis.py
+++ AnalysisStep/test/MasterPy/ZZ4lAnalysis.py
@@ -974,36 +974,41 @@ UPDATE_JETS = False #FIXME: deactivated for now; what should be done for RunII ?
 ### Load JEC
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 from CondCore.DBCommon.CondDBSetup_cfi import *
-process.jec = cms.ESSource("PoolDBESSource",
-      DBParameters = cms.PSet(
-        messageLevel = cms.untracked.int32(1)
+
+if IsMC: 
+    process.jec = cms.ESSource("PoolDBESSource",
+        DBParameters = cms.PSet(
+            messageLevel = cms.untracked.int32(1)
         ),
-      timetype = cms.string('runnumber'),
-      toGet = cms.VPSet(
-      cms.PSet(
+        timetype = cms.string('runnumber'),
+        toGet = cms.VPSet(
+        cms.PSet(
             record = cms.string('JetCorrectionsRecord'),
             tag    = cms.string('JetCorrectorParametersCollection_Fall15_25nsV2_MC_AK4PFchs'),
             label  = cms.untracked.string('AK4PFchs')
             ),
-      #cms.PSet(
-      #      record = cms.string('JetCorrectionsRecord'),
-      #      tag    = cms.string('JetCorrectorParametersCollection_Summer12_V7_DATA_AK5Calo'),
-      #      # tag    = cms.string('JetCorrectorParametersCollection_Summer12_V7_DATA_AK5Calo'),
-      #      label  = cms.untracked.string('AK5Calo')
-      #      ),
-      #..................................................
-      ## here you add as many jet types as you need
-      ## note that the tag name is specific for the particular sqlite file 
-      ), 
-      connect = cms.string('sqlite_fip:ZZAnalysis/AnalysisStep/data/JEC/Fall15_25nsV2_MC.db'),
-      #connect = cms.string('sqlite_file:/afs/cern.ch/user/h/heli/work/private/cms/hzz_cjlst/framework/CMSSW_7_6_3_patch2_v2/src/ZZAnalysis/AnalysisStep/data/JEC/Fall15_25nsV2_MC.db'),
-     # uncomment above tag lines and this comment to use MC JEC
-     # connect = cms.string('sqlite:Summer12_V7_MC.db')
-)
+        ), 
+        connect = cms.string('sqlite_fip:ZZAnalysis/AnalysisStep/data/JEC/Fall15_25nsV2_MC.db'),
+    )
+else: 
+    process.jec = cms.ESSource("PoolDBESSource",
+        DBParameters = cms.PSet(
+            messageLevel = cms.untracked.int32(1)
+        ),
+        timetype = cms.string('runnumber'),
+        toGet = cms.VPSet(
+        cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('JetCorrectorParametersCollection_Fall15_25nsV2_DATA_AK4PFchs'),
+            label  = cms.untracked.string('AK4PFchs')
+            ),
+        ), 
+        connect = cms.string('sqlite_fip:ZZAnalysis/AnalysisStep/data/JEC/Fall15_25nsV2_DATA.db'),
+    )
 ## add an es_prefer statement to resolve a possible conflict from simultaneous connection to a global tag
 process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
 
-### JEC
+### JEC re-apply
 from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetCorrFactorsUpdated
 process.patJetCorrFactorsReapplyJEC = patJetCorrFactorsUpdated.clone(
   src = cms.InputTag("slimmedJets"),
@@ -1018,7 +1023,6 @@ process.patJetsReapplyJEC = patJetsUpdated.clone(
   jetCorrFactorsSource = cms.VInputTag(cms.InputTag("patJetCorrFactorsReapplyJEC"))
   )
 
-#process.p += cms.Sequence( process.patJetCorrFactorsReapplyJEC + process. patJetsReapplyJEC )
 
 
 ###
