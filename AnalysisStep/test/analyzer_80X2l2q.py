303bc021169e7e319195f7960ef46a661dd2581f
Roberto <roberto.covarelli@cern.ch>
2016-05-30 13:59:04 +0200

    Fix several bugs

---
+++ AnalysisStep/test/analyzer_80X2l2q.py
@@ -0,0 +1,104 @@
+
+LEPTON_SETUP = 2015
+PD = ""
+MCFILTER = ""
+ELECORRTYPE = "RunII" # "None", "Moriond", "Paper", or "RunII"
+ELEREGRESSION = "Paper" # "None", "Moriond", "PaperNoComb", or "Paper" 
+APPLYMUCORR = True
+BUNCH_SPACING = 25
+#FSRMODE = "Legacy" # Legacy or Run II
+
+CRSync = False #add CR paths
+
+#KEEPLOOSECOMB = True # Do not skip loose lepton ZZ combinations (for debugging
+
+#For DATA: 
+#IsMC = False
+#PD = "DoubleEle"
+
+# Get absolute path
+import os
+PyFilePath = os.environ['CMSSW_BASE'] + "/src/ZZAnalysis/AnalysisStep/test/"
+
+### ----------------------------------------------------------------------
+### Standard sequence
+### ----------------------------------------------------------------------
+
+execfile(PyFilePath + "analyzer2l2q.py")
+
+process.source.inputCommands = cms.untracked.vstring("keep *", "drop LHERunInfoProduct_*_*_*", "drop LHEEventProduct_*_*_*")
+
+### ----------------------------------------------------------------------
+### Replace parameters
+### ----------------------------------------------------------------------
+
+process.source.fileNames = cms.untracked.vstring(
+
+     #'/store/mc/RunIIFall15MiniAODv1/BulkGravToZZToZlepZhad_narrow_M-800_13TeV-madgraph/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/20000/14E8B66A-E5B0-E511-8CDC-B083FED177B1.root'
+    '/store/mc/RunIISpring16MiniAODv1/Graviton2PBqqbarToZZTo4L_width0p3_M-750_13TeV-JHUgenV6-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_v3-v1/70000/0CB9721B-0623-E611-9466-0025905505BE.root'
+
+  
+
+    )
+
+process.calibratedPatElectrons.isSynchronization = cms.bool(True)
+process.calibratedMuons.isSynchronization = cms.bool(True)
+
+process.maxEvents.input = -1
+#process.source.skipEvents = cms.untracked.uint32(5750)
+
+# Silence output
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+
+### ----------------------------------------------------------------------
+### Analyzer for Plots
+### ----------------------------------------------------------------------
+
+
+#process.source.eventsToProcess = cms.untracked.VEventRange("1:122490", "1:1343", "1:177684")
+
+# Debug
+process.dumpUserData =  cms.EDAnalyzer("dumpUserData",
+      dump = cms.untracked.bool(True),
+     dumpTrigger = cms.untracked.bool(True),
+#     muonSrc = cms.InputTag("slimmedMuons"),
+#     electronSrc = cms.InputTag("slimmedElectrons"),
+     muonSrc = cms.InputTag("appendPhotons:muons"), 
+     electronSrc = cms.InputTag("appendPhotons:electrons"),
+     candidateSrcs = cms.PSet(
+     
+   #       Zb     = cms.InputTag("bareZCand"),                                  
+   #     Zjjb  = cms.InputTag("bareZjjCand"),     
+        Z     = cms.InputTag("ZCand"),                                  
+        ZZ  = cms.InputTag("ZZCand"),
+       ZZfat  = cms.InputTag("ZZCandFat"),
+#         ZZb  = cms.InputTag("bareZZCand"),
+#        ZL  = cms.InputTag("ZlCand"),
+     ),
+  #   jetSrc = cms.InputTag("cleanJets"),
+)
+
+# Create lepton sync file
+#process.PlotsZZ.dumpForSync = True;
+#process.p = cms.EndPath( process.PlotsZZ)
+
+# Keep all events in the tree, even if no candidate is selected
+#process.ZZTree.skipEmptyEvents = False
+
+
+# Also process CRs
+if (CRSync) :
+    process.CRPath = cms.Path(process.CR)
+    process.CRtrees = cms.EndPath(process.CRZLLTree + process.CRZLTree)
+
+# replace the paths in analyzer.py
+process.trees = cms.EndPath(process.ZZTree)
+
+#Dump reconstructed variables
+#process.appendPhotons.debug = cms.untracked.bool(True)
+#process.dump = cms.Path(process.dumpUserData)
+
+#Print MC history
+#process.mch = cms.EndPath(process.printTree)
