b5dce1fff356c82c5a0034bb18488609612d8a59
Roberto <roberto.covarelli@cern.ch>
2016-06-29 21:32:27 +0200

    correct use of covariance matrix

--- AnalysisStep/test/Plotter/fitAlphaMethod_2l2q.C
+++ AnalysisStep/test/Plotter/fitAlphaMethod_2l2q.C
@@ -31,6 +31,8 @@
 #include "TSystem.h"
 #include "TTree.h"
 #include "TChain.h"
+#include "TMatrix.h"
+#include "TFitResult.h"
 #include "TMVA/Tools.h"
 #include "TMVA/Reader.h"
 
@@ -48,23 +50,64 @@ using namespace std;
 const int nChannels = 6;
 string channelSPart1[nChannels] = {"resolved","merged","resolved","merged","resolved","merged"};
 string channelSPart2[nChannels] = {"","","btag","btag","vbf","vbf"};
-float minX[nChannels] = {450.,500.,450.,600.,450.,600.};
-float maxX[nChannels] = {2000.,1600.,1200.,1200.,900.,900.};
-int binsForAlpha[nChannels] = {1,3,4,10,4,4};
+float minX[nChannels] = {450.,600.,450.,600.,450.,600.};
+float maxX[nChannels] = {2000.,2000.,1600.,1600.,1600.,1600.};
+int binsForAlpha[nChannels] = {2,3,4,10,3,3};
 
 Double_t myfunction(Double_t *x, Double_t *par)
 {
    Float_t xx =x[0];
-   Double_t f = par[0]*exp(-par[1]*xx);
+   Double_t f = par[0]*exp(-par[1]*xx)+par[2]*exp(-par[3]*xx);
    return f;
 }
 
-Double_t myfunction2(Double_t *x, Double_t *par)
+Double_t myfunctionErrUp(Double_t *x, Double_t *par)
+{
+   Float_t xx =x[0];
+   Double_t f = par[0]*exp(-par[1]*xx)+par[2]*exp(-par[3]*xx);
+
+   Double_t der[4]; 
+   der[0] = exp(-par[1]*xx);
+   der[1] = -par[0]*xx*exp(-par[1]*xx);   
+   der[2] = exp(-par[3]*xx);
+   der[3] = -par[2]*xx*exp(-par[3]*xx);
+
+   Double_t fsigma = 0.;
+   for (int j=0; j<4; j++) {
+     for (int i=0; i<4; i++) {
+       fsigma += der[i]*par[4+j+4*i]*der[j];
+     }   
+   }
+   return f+sqrt(fabs(fsigma));
+}
+
+Double_t myfunctionErrDown(Double_t *x, Double_t *par)
+{
+   Float_t xx =x[0];
+   Double_t f = par[0]*exp(-par[1]*xx)+par[2]*exp(-par[3]*xx);
+
+   Double_t der[4]; 
+   der[0] = exp(-par[1]*xx);
+   der[1] = -par[0]*xx*exp(-par[1]*xx);   
+   der[2] = exp(-par[3]*xx);
+   der[3] = -par[2]*xx*exp(-par[3]*xx);
+
+   Double_t fsigma = 0.;
+   for (int j=0; j<4; j++) {
+     for (int i=0; i<4; i++) {
+       fsigma += der[i]*par[4+j+4*i]*der[j];
+     }   
+   }
+   return f-sqrt(fabs(fsigma));
+}
+
+/* Double_t myfunction2(Double_t *x, Double_t *par)
 {
    Float_t xx =x[0];
    Double_t f = (par[0]+par[2]*xx)*exp(-par[1]*xx);
    return f;
-}
+   } 
+*/
 
 void fitAlphaMethod_2l2q(string dirout = "fitAlpha", string theNtuple = "TMVAAndRoofitInputs.root")
 {
@@ -80,14 +123,14 @@ void fitAlphaMethod_2l2q(string dirout = "fitAlpha", string theNtuple = "TMVAAnd
   TF1* ffitdown[nChannels];
 
   for (int tc=0; tc<nChannels; tc++) {
-    char histoName[100];
+    char histoName[1000];
     sprintf(histoName,"hmass_%sSB%s_Data",channelSPart1[tc].c_str(),channelSPart2[tc].c_str());
     TH1F* fitHist = (TH1F*)inFile->Get(histoName);
     sprintf(histoName,"hmass_%sSB%s_TT",channelSPart1[tc].c_str(),channelSPart2[tc].c_str());
     TH1F* ttWZZZHist = (TH1F*)inFile->Get(histoName); 
     sprintf(histoName,"hmass_%sSB%s_DY",channelSPart1[tc].c_str(),channelSPart2[tc].c_str());
     TH1F* alphaDen = (TH1F*)inFile->Get(histoName);
-    sprintf(histoName,"hmass_%sSR%s_Data",channelSPart1[tc].c_str(),channelSPart2[tc].c_str());
+    sprintf(histoName,"hmass_%sSR%s_DY",channelSPart1[tc].c_str(),channelSPart2[tc].c_str());
     TH1F* alphaNum = (TH1F*)inFile->Get(histoName);
 
     // "rebin" alpha histograms if not enough events
@@ -149,45 +192,54 @@ void fitAlphaMethod_2l2q(string dirout = "fitAlpha", string theNtuple = "TMVAAnd
     c1.SaveAs(histoName);
     
     fitHist->Add(fitHist,ttWZZZHist,1.,-1.);
-    fitHist->Multiply(fitHist,alphaDenRebin);
+    fitHist->Multiply(fitHist,alphaNumRebin);
 
     sprintf(histoName,"ffit%d",tc);
-    if (tc == 1) ffit[tc] = new TF1(histoName,myfunction2,minX[tc],maxX[tc],3);
-    else ffit[tc] = new TF1(histoName,myfunction,minX[tc],maxX[tc],2);
-    ffit[tc]->SetParNames("constant","slope");
+    // if (tc == 1) ffit[tc] = new TF1(histoName,myfunction2,minX[tc],maxX[tc],3); else
+    ffit[tc] = new TF1(histoName,myfunction,minX[tc],maxX[tc],4);
+    ffit[tc]->SetParNames("constant","slope","constant2","slope2");
     ffit[tc]->SetLineColor(2);
     ffit[tc]->SetLineStyle(kDashed);
     ffit[tc]->SetLineWidth(3);
     ffit[tc]->SetParameter(0,fitHist->Integral());
     ffit[tc]->SetParameter(1,0.005);
+    ffit[tc]->SetParameter(2,fitHist->Integral()/10.);
+    ffit[tc]->SetParameter(3,0.01);
+    if (tc>0) {
+      ffit[tc]->FixParameter(2,0.);
+      ffit[tc]->FixParameter(3,0.01);
+    }      
 
     c1.cd();
-    fitHist->Fit(histoName,"","",minX[tc],maxX[tc]);
+    TFitResultPtr frp = fitHist->Fit(histoName,"S","",minX[tc],maxX[tc]);
+    TMatrixD cov = frp->GetCovarianceMatrix();
+    cov.Print();
+    double* covElem = cov.GetMatrixArray(); 
 
+    // Build upper and lower 1sigma function
     sprintf(histoName,"ffitup%d",tc);
-    if (tc == 1) ffitup[tc] = new TF1(histoName,myfunction2,minX[tc],maxX[tc],3);
-    else ffitup[tc] = new TF1(histoName,myfunction,minX[tc],maxX[tc],2);
-    
-    ffitup[tc]->SetParameter(0,TMath::Max(0.,ffit[tc]->GetParameter(0)+ffit[tc]->GetParError(0)));
-    // ffitup[tc]->SetParameter(1,ffit[tc]->GetParameter(1)+ffit[tc]->GetParError(1));
-    ffitup[tc]->SetParameter(1,ffit[tc]->GetParameter(1));
-    if (tc == 1) ffitup[tc]->SetParameter(2,ffit[tc]->GetParameter(2)); 
-    ffitup[tc]->SetParNames("constant","slope");
+    ffitup[tc] = new TF1(histoName,myfunctionErrUp,minX[tc],maxX[tc],20);
+    for (int j=0; j<4; j++) {
+      ffitup[tc]->FixParameter(j,ffit[tc]->GetParameter(j));
+      for (int i=0; i<4; i++) {
+	ffitup[tc]->FixParameter(4+j+4*i,covElem[4*j+i]);   // array is filled per column
+                                                            // not per row
+      }
+    }
     ffitup[tc]->SetLineColor(4);
     ffitup[tc]->SetLineStyle(kDashed);
     ffitup[tc]->SetLineWidth(3);  
-
     ffitup[tc]->Draw("same");
 
     sprintf(histoName,"ffitdown%d",tc);
-    if (tc == 1) ffitdown[tc] = new TF1(histoName,myfunction2,minX[tc],maxX[tc],3);
-    else ffitdown[tc] = new TF1(histoName,myfunction,minX[tc],maxX[tc],2);
-    
-    ffitdown[tc]->SetParameter(0,TMath::Max(0.,ffit[tc]->GetParameter(0)-ffit[tc]->GetParError(0)));
-    // ffitdown[tc]->SetParameter(1,ffit[tc]->GetParameter(1)-ffit[tc]->GetParError(1));
-    ffitdown[tc]->SetParameter(1,ffit[tc]->GetParameter(1));
-    if (tc == 1) ffitdown[tc]->SetParameter(2,ffit[tc]->GetParameter(2)); 
-    ffitdown[tc]->SetParNames("constant","slope");
+    ffitdown[tc] = new TF1(histoName,myfunctionErrDown,minX[tc],maxX[tc],20);
+    for (int j=0; j<4; j++) {
+      ffitdown[tc]->FixParameter(j,ffit[tc]->GetParameter(j));
+      for (int i=0; i<4; i++) {
+	ffitdown[tc]->FixParameter(4+j+4*i,covElem[4*j+i]);   // array is filled per column
+                                                              // not per row
+      }
+    }
     ffitdown[tc]->SetLineColor(4);
     ffitdown[tc]->SetLineStyle(kDashed);
     ffitdown[tc]->SetLineWidth(3);
@@ -196,5 +248,11 @@ void fitAlphaMethod_2l2q(string dirout = "fitAlpha", string theNtuple = "TMVAAnd
     
     sprintf(histoName,"~/www/graviton/%s/bkgshapeAlphaMethod_%s%s.png",dirout.c_str(),channelSPart1[tc].c_str(),channelSPart2[tc].c_str()); 
     c1.SaveAs(histoName);
+    if (tc == 0) {
+      gPad->SetLogy(1);
+      sprintf(histoName,"~/www/graviton/%s/bkgshapeAlphaMethod_%s%s_log.png",dirout.c_str(),channelSPart1[tc].c_str(),channelSPart2[tc].c_str());
+      c1.SaveAs(histoName);
+      gPad->SetLogy(0);
+    }
   }
 }
