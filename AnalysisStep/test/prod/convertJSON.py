534cba62c4655035550d6cbd2ea056f800d48565
Nicola Amapane <nicola.amapane@cern.ch>
2016-07-06 18:54:45 +0200

    update JSON (still not final)

--- AnalysisStep/test/prod/convertJSON.py
+++ AnalysisStep/test/prod/convertJSON.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
-filename = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt'
+#filename = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-273730_13TeV_PromptReco_Collisions16_JSON.txt'
+filename = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-275783_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt' #Jul 1, 2016; 5.76/fb; https://hypernews.cern.ch/HyperNews/CMS/get/physics-validation/2675.html
+
 
 json = eval( open(filename).read() )
 
