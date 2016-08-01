e9848cfa47fb33a830548cbd144753cd23a7ec59
Can You <cyou5@jhu.edu>
2016-08-02 00:01:43 +0200

    2016 setup

---
+++ AnalysisStep/test/prod/analyzer_20162l2q.py
@@ -0,0 +1,28 @@
+#----------------------------------------------------------------------
+#
+# Configuration for data, DoubleMu stream
+#
+#----------------------------------------------------------------------
+
+LEPTON_SETUP = 2016
+
+ELECORRTYPE = "RunII"
+ELEREGRESSION = "None"
+APPLYMUCORR = True
+
+FSRMODE = "RunII"
+
+# Load deafult job config
+import os
+PyFilePath = os.environ['CMSSW_BASE'] + "/src/ZZAnalysis/AnalysisStep/test/"
+execfile(PyFilePath + "analyzer2l2q.py")        
+
+# Silence output
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+#process.appendPhotons.photonSel="skip"
+
+#process.appendPhotons.photonSel = "passThrough"
+#process.FSR  = cms.EDAnalyzer("FSRAnalyzer",)
+#process.FSRPath = cms.EndPath(process.FSR)
