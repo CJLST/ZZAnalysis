ee2e3b6315b4c7aa1dca7c4f5468b8d901b0dff5
Ulascan Sarica <ulascan90@gmail.com>
2016-11-15 14:15:09 +0100

    Revert changes to classes

--- AnalysisStep/src/classes.h
+++ AnalysisStep/src/classes.h
@@ -4,11 +4,3 @@
 
 edm::Ptr<pat::PFParticle> dummy1;
 pat::UserHolder<std::vector<edm::Ptr<pat::PFParticle> > > dummy2;
-
-BranchHelpers::ExtendedBranch<Bool_t> bdummy_b(0,"",(Bool_t)false, false);
-BranchHelpers::ExtendedBranch<Char_t> bdummy_c(0, "", (Char_t)' ', false);
-BranchHelpers::ExtendedBranch<Short_t> bdummy_s(0, "", (Short_t)0, false);
-BranchHelpers::ExtendedBranch<Int_t> bdummy_i(0, "", (Int_t)0, false);
-BranchHelpers::ExtendedBranch<Long64_t> bdummy_l(0, "", (Long64_t)0, false);
-BranchHelpers::ExtendedBranch<Float_t> bdummy_f(0, "", (Float_t)0, false);
-BranchHelpers::ExtendedBranch<Double_t> bdummy_d(0, "", (Double_t)0, false);
