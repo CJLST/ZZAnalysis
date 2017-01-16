a6151087182978dff45dccc4e7dc36acfc3c3f92
Heshy Roskes <jroskes1@jhu.edu>
2017-01-16 18:53:28 +0100

    don't use ROOT!

--- AnalysisStep/python/miscenums.py
+++ AnalysisStep/python/miscenums.py
@@ -5,12 +5,6 @@ Any enums defined in interface/miscenums.h will also be accessible here.
 def _():
     """put all the dirty stuff inside a function so it doesn't get imported with import *"""
     import os, re
-    import ROOT
-
-    def include(filename):
-      ROOT.gROOT.ProcessLine("#include <{}>".format(filename))
-
-    include("ZZAnalysis/AnalysisStep/interface/miscenums.h")
 
     with open(os.path.join(os.environ["CMSSW_BASE"], "src/ZZAnalysis/AnalysisStep/interface/miscenums.h")) as f:
         contents = f.read()
@@ -28,12 +22,13 @@ def _():
                        , contents)
 
 
+    result = []
     for enumname, enumcontents in enums:
-        globals()[enumname] = getattr(ROOT, enumname)
         enumcontents = enumcontents.split(",")
-        enumitems = [content.split("=")[0].strip() for content in enumcontents]
-        for itemname in enumitems:
-            globals()[itemname] = getattr(ROOT, itemname)
+        for content in enumcontents:
+            result.append(content.strip())
+    return result
 
-_()
-del _
+for enum in _():
+    exec enum
+del enum, _
