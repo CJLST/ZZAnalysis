8e0d8edd1451aba831d67cf4f560e985474ea8ab
Philipp <p.pigard@gmail.com>
2017-01-27 09:10:02 +0100

    Improve handling of VBS analysis details

---
+++ AnalysisStep/test/prod/analyzer_2017_VBS.py
@@ -0,0 +1,27 @@
+#----------------------------------------------------------------------
+#
+# Configuration for data, DoubleMu stream
+#
+#----------------------------------------------------------------------
+
+
+# Load deafult job config
+import os
+PyFilePath = os.environ['CMSSW_BASE'] + "/src/ZZAnalysis/AnalysisStep/test/"
+execfile(PyFilePath + "analyzer.py")        
+
+#declareDefault("BESTCANDCOMPARATOR", "byBestZ1bestZ2", globals())
+
+# use SMP ZZ arbitrator (does not alter RSE/TLE)
+process.ZLLCand.bestCandComparator = cms.string('byBestZ1bestZ2')
+process.ZZCand.bestCandComparator = cms.string('byBestZ1bestZ2')
+
+# Silence output
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+#process.appendPhotons.photonSel="skip"
+
+#process.appendPhotons.photonSel = "passThrough"
+#process.FSR  = cms.EDAnalyzer("FSRAnalyzer",)
+#process.FSRPath = cms.EndPath(process.FSR)
