f6dab55f7aae044bd8ca0956ac1b303849c02841
Simon <simon.regnard@cern.ch>
2017-02-13 22:44:27 +0100

    New categorization function (add VHMETTagged, refine VHHadrTagged) ; cleanup categorization file.

--- AnalysisStep/src/Category.cc
+++ AnalysisStep/src/Category.cc
@@ -6,100 +6,15 @@
 
 #include "TMath.h"
 #include "TRandom3.h"
-#include "Math/GenVector/LorentzVector.h"
-#include "Math/GenVector/PtEtaPhiM4D.h"
-
-typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> >  LV;
 
 
-int flagDijetVH(
-		int nCleanedJetsPt30, 
-		float* jetpt,
-		float* jeteta,
-		float* jetphi,
-		float* jetmass
-		)
+
+extern "C" int categoryLegacy( int nCleanedJetsPt30 )
 {
-
-  bool found = false;
-
-  if(nCleanedJetsPt30>=2){
-
-    for(int j1=0; j1<nCleanedJetsPt30; j1++){
-      if( std::abs(jeteta[j1])<2.4 && jetpt[j1]>40. ){
-
-	for(int j2=j1+1; j2<nCleanedJetsPt30; j2++){
-	  if( std::abs(jeteta[j2])<2.4 && jetpt[j2]>40. ){
-
-	    LV jet1 (jetpt[j1],jeteta[j1],jetphi[j1],jetmass[j1]);
-	    LV jet2 (jetpt[j2],jeteta[j2],jetphi[j2],jetmass[j2]);
-	    float mjj = (jet1+jet2).mass();
-
-	    if( 60.<mjj && mjj<120. ){
-	      found = true;
-	      break;
-	    }
-
-	  }
-	}
-
-	if(found) break;
-      }
-    }
-
-  }
-  
-  return found;
-
-}
-
-
-//int category(
-extern "C" int category(
-	     int nExtraLep,
-	     float ZZPt,
-	     float ZZMass,
-	     int nCleanedJetsPt30, 
-	     int nCleanedJetsPt30BTagged,
-	     float* jetpt,
-	     float* jeteta,
-	     float* jetphi,
-	     float* jetmass,
-	     float DiJetFisher
-	     )
-{
-
-  int category = -1;
-
-  if( nExtraLep==0 && nCleanedJetsPt30>=2 && nCleanedJetsPt30BTagged<=1 && DiJetFisher>0.5 ){
-
-    category = VBFTagged; 
-
-  }else if( ( nExtraLep==0 && nCleanedJetsPt30>=2 && ZZPt>ZZMass && flagDijetVH(nCleanedJetsPt30,jetpt,jeteta,jetphi,jetmass) )
-            || ( nExtraLep==0 && nCleanedJetsPt30==2 && nCleanedJetsPt30BTagged==2 ) ){
-
-    category = VHHadrTagged;
-
-  }else if( nExtraLep>=1 && nCleanedJetsPt30<=2 && nCleanedJetsPt30BTagged==0 ){
-
-    category = VHLeptTagged;
-
-  }else if( nExtraLep>=1 || (nCleanedJetsPt30>=3 && nCleanedJetsPt30BTagged>=1) ){
-
-    category = ttHTagged;
-
-  }else if(nCleanedJetsPt30>=1){
-
-    category = OneJetTagged;
-
-  }else{
-
-    category = Untagged;
-
-  }
-
-  return category;
-
+  if(nCleanedJetsPt30>=2)
+    return Dijet;
+  else
+    return ZeroOneJet;
 }
 
 
@@ -119,22 +34,22 @@ extern "C" int categoryMor16(
 
 
 extern "C" int categoryIchep16(
-	     int nExtraLep,
-	     int nExtraZ,
-	     int nCleanedJetsPt30, 
-	     int nCleanedJetsPt30BTagged,
-	     float* jetQGLikelihood,
-	     float p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal,
-	     float p_JQCD_SIG_ghg2_1_JHUGen_JECNominal,
-       float p_JJVBF_SIG_ghv1_1_JHUGen_JECNominal,
-       float p_JVBF_SIG_ghv1_1_JHUGen_JECNominal,
-       float pAux_JVBF_SIG_ghv1_1_JHUGen_JECNominal,
-	     float p_HadWH_SIG_ghw1_1_JHUGen_JECNominal,
-	     float p_HadZH_SIG_ghz1_1_JHUGen_JECNominal,
-             float* jetPhi,
-	     float ZZMass,
-	     bool useQGTagging 
-	     )
+			       int nExtraLep,
+			       int nExtraZ,
+			       int nCleanedJetsPt30, 
+			       int nCleanedJetsPt30BTagged_bTagSF,
+			       float* jetQGLikelihood,
+			       float p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal,
+			       float p_JQCD_SIG_ghg2_1_JHUGen_JECNominal,
+			       float p_JJVBF_SIG_ghv1_1_JHUGen_JECNominal,
+			       float p_JVBF_SIG_ghv1_1_JHUGen_JECNominal,
+			       float pAux_JVBF_SIG_ghv1_1_JHUGen_JECNominal,
+			       float p_HadWH_SIG_ghw1_1_JHUGen_JECNominal,
+			       float p_HadZH_SIG_ghz1_1_JHUGen_JECNominal,
+			       float* jetPhi,
+			       float ZZMass,
+			       bool useQGTagging 
+			       )
 {
 
   float D_VBF2j = -2;
@@ -168,21 +83,21 @@ extern "C" int categoryIchep16(
 
     return VBF1jTaggedIchep16;
 
-  }else if( nExtraLep==0 && (((nCleanedJetsPt30==2||nCleanedJetsPt30==3)&&nCleanedJetsPt30BTagged<=1)||(nCleanedJetsPt30>=4&&nCleanedJetsPt30BTagged==0)) && D_VBF2j>WP_VBF2j ){
+  }else if( nExtraLep==0 && (((nCleanedJetsPt30==2||nCleanedJetsPt30==3)&&nCleanedJetsPt30BTagged_bTagSF<=1)||(nCleanedJetsPt30>=4&&nCleanedJetsPt30BTagged_bTagSF==0)) && D_VBF2j>WP_VBF2j ){
 
     return VBF2jTaggedIchep16;
 
-  }else if(   ( nExtraLep==0 && (((nCleanedJetsPt30==2||nCleanedJetsPt30==3)&&nCleanedJetsPt30BTagged<=1)||(nCleanedJetsPt30>=4&&nCleanedJetsPt30BTagged==0)) && (D_WHh>WP_WHh||D_ZHh>WP_ZHh) )
-	   || ( nExtraLep==0 && (nCleanedJetsPt30==2||nCleanedJetsPt30==3) && nCleanedJetsPt30BTagged>=2 ) ){
+  }else if(   ( nExtraLep==0 && (((nCleanedJetsPt30==2||nCleanedJetsPt30==3)&&nCleanedJetsPt30BTagged_bTagSF<=1)||(nCleanedJetsPt30>=4&&nCleanedJetsPt30BTagged_bTagSF==0)) && (D_WHh>WP_WHh||D_ZHh>WP_ZHh) )
+	   || ( nExtraLep==0 && (nCleanedJetsPt30==2||nCleanedJetsPt30==3) && nCleanedJetsPt30BTagged_bTagSF>=2 ) ){
 
     return VHHadrTaggedIchep16;
 
-  }else if(   ( nCleanedJetsPt30<=3 && nCleanedJetsPt30BTagged==0 && (nExtraLep==1||nExtraZ>=1) )
+  }else if(   ( nCleanedJetsPt30<=3 && nCleanedJetsPt30BTagged_bTagSF==0 && (nExtraLep==1||nExtraZ>=1) )
 	   || ( nCleanedJetsPt30==0 && nExtraLep>=1 ) ){
 
     return VHLeptTaggedIchep16;
 
-  }else if(   ( nCleanedJetsPt30>=4 && nCleanedJetsPt30BTagged>=1 )
+  }else if(   ( nCleanedJetsPt30>=4 && nCleanedJetsPt30BTagged_bTagSF>=1 )
 	   || nExtraLep>=1 ){
 
     return ttHTaggedIchep16;
@@ -196,10 +111,84 @@ extern "C" int categoryIchep16(
 }
 
 
-extern "C" int categoryLegacy( int nCleanedJetsPt30 )
+extern "C" int categoryMor17(
+			     int nExtraLep,
+			     int nExtraZ,
+			     int nCleanedJetsPt30, 
+			     int nCleanedJetsPt30BTagged_bTagSF,
+			     float* jetQGLikelihood,
+			     float p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal,
+			     float p_JQCD_SIG_ghg2_1_JHUGen_JECNominal,
+			     float p_JJVBF_SIG_ghv1_1_JHUGen_JECNominal,
+			     float p_JVBF_SIG_ghv1_1_JHUGen_JECNominal,
+			     float pAux_JVBF_SIG_ghv1_1_JHUGen_JECNominal,
+			     float p_HadWH_SIG_ghw1_1_JHUGen_JECNominal,
+			     float p_HadZH_SIG_ghz1_1_JHUGen_JECNominal,
+			     float* jetPhi,
+			     float ZZMass,
+			     float PFMET,
+			     bool useVHMETTagged,
+			     bool useQGTagging
+			     )
 {
-  if(nCleanedJetsPt30>=2)
-    return Dijet;
-  else
-    return ZeroOneJet;
+
+  float D_VBF2j = -2;
+  float D_VBF1j = -2;
+  float D_WHh   = -2;
+  float D_ZHh   = -2;
+  if(useQGTagging){
+    if(nCleanedJetsPt30==1){
+      D_VBF1j = DVBF1j_ME_QG(p_JVBF_SIG_ghv1_1_JHUGen_JECNominal, pAux_JVBF_SIG_ghv1_1_JHUGen_JECNominal, p_JQCD_SIG_ghg2_1_JHUGen_JECNominal, ZZMass, jetQGLikelihood, jetPhi);
+    }else if(nCleanedJetsPt30>=2){
+      D_VBF2j = DVBF2j_ME_QG(p_JJVBF_SIG_ghv1_1_JHUGen_JECNominal, p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal, ZZMass, jetQGLikelihood, jetPhi);
+      D_WHh   = DWHh_ME_QG(p_HadWH_SIG_ghw1_1_JHUGen_JECNominal, p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal, ZZMass, jetQGLikelihood, jetPhi);
+      D_ZHh   = DZHh_ME_QG(p_HadZH_SIG_ghz1_1_JHUGen_JECNominal, p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal, ZZMass, jetQGLikelihood, jetPhi);
+    }
+  }else{
+    if(nCleanedJetsPt30==1){
+      D_VBF1j = DVBF1j_ME(p_JVBF_SIG_ghv1_1_JHUGen_JECNominal, pAux_JVBF_SIG_ghv1_1_JHUGen_JECNominal, p_JQCD_SIG_ghg2_1_JHUGen_JECNominal, ZZMass);
+    }else if(nCleanedJetsPt30>=2){
+      D_VBF2j = DVBF2j_ME(p_JJVBF_SIG_ghv1_1_JHUGen_JECNominal, p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal, ZZMass);
+      D_WHh   = DWHh_ME(p_HadWH_SIG_ghw1_1_JHUGen_JECNominal, p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal, ZZMass);
+      D_ZHh   = DZHh_ME(p_HadZH_SIG_ghz1_1_JHUGen_JECNominal, p_JJQCD_SIG_ghg2_1_JHUGen_JECNominal, ZZMass);
+    }
+  }
+
+  float WP_VBF2j = getDVBF2jetsWP(ZZMass, useQGTagging);
+  float WP_VBF1j = getDVBF1jetWP(ZZMass, useQGTagging);
+  float WP_WHh = getDWHhWP(ZZMass, useQGTagging);
+  float WP_ZHh = getDZHhWP(ZZMass, useQGTagging);
+
+  if( nExtraLep==0 && (((nCleanedJetsPt30==2||nCleanedJetsPt30==3)&&nCleanedJetsPt30BTagged_bTagSF<=1)||(nCleanedJetsPt30>=4&&nCleanedJetsPt30BTagged_bTagSF==0)) && D_VBF2j>WP_VBF2j ){
+
+    return VBF2jTaggedMor17;
+
+  }else if( nExtraLep==0 && (nCleanedJetsPt30==2||nCleanedJetsPt30==3||(nCleanedJetsPt30>=4&&nCleanedJetsPt30BTagged_bTagSF==0)) && (D_WHh>WP_WHh||D_ZHh>WP_ZHh) ){
+
+    return VHHadrTaggedMor17;
+
+  }else if(   ( nCleanedJetsPt30<=3 && nCleanedJetsPt30BTagged_bTagSF==0 && (nExtraLep==1||nExtraZ>=1) )
+	   || ( nCleanedJetsPt30==0 && nExtraLep>=1 ) ){
+
+    return VHLeptTaggedMor17;
+
+  }else if(   ( nCleanedJetsPt30>=4 && nCleanedJetsPt30BTagged_bTagSF>=1 )
+	   || nExtraLep>=1 ){
+
+    return ttHTaggedMor17;
+  
+  }else if( useVHMETTagged && nExtraLep==0 && (nCleanedJetsPt30==0||nCleanedJetsPt30==1) && PFMET>100 ){
+
+    return VHMETTaggedMor17;
+
+  }else if( nExtraLep==0 && nCleanedJetsPt30==1 && D_VBF1j>WP_VBF1j ){
+
+    return VBF1jTaggedMor17;
+
+  }else{
+
+    return UntaggedMor17;
+
+  }
+
 }
