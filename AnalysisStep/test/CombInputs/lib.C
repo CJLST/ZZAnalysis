e58070c0ea5fb8d8c534c81d5b087b29c7553f6b
muhammad Bilal Manzoor Kiani <mkiani@cern.ch>
2017-04-07 11:37:36 +0200

    Adding HiggsAnalysis/CombinedLimit package

--- AnalysisStep/test/CombInputs/lib.C
+++ AnalysisStep/test/CombInputs/lib.C
@@ -7,7 +7,7 @@
 void lib(){
 gSystem->Load("libRooFit.so");
 gSystem->Load("libZZMatrixElementMELA.so");
-//gSystem->Load("libHiggsAnalysisCombinedLimit.so");
+gSystem->Load("libHiggsAnalysisCombinedLimit.so");
 gSystem->Load("libZZAnalysisAnalysisStep.so");
 gSystem->AddIncludePath("-I$ROOFITSYS");
 gSystem->AddIncludePath("-I$CMSSW_BASE");
