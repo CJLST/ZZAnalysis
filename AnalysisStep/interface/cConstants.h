290c0104802ef92710ed6fc8f65628eece7a67ba
Heshy Roskes <jroskes1@jhu.edu>
2017-07-28 16:57:58 -0400

    new c constants

--- AnalysisStep/interface/cConstants.h
+++ AnalysisStep/interface/cConstants.h
@@ -1,6 +1,20 @@
 #ifndef CCONSTANTS_H
 #define CCONSTANTS_H
 
+#include "TSpline3.h"
+#include "TString.h"
+
+class cConstantSpline {
+  public:
+    cConstantSpline(const TString& filename);
+    void initspline();
+    double eval(double ZZMass);
+  private:
+    const TString filename_;
+    unique_ptr<TFile> f_;
+    TSpline3* spline_;
+};
+
 extern "C" float getDVBF2jetsConstant(float ZZMass);
 extern "C" float getDVBF1jetConstant(float ZZMass);
 extern "C" float getDWHhConstant(float ZZMass);
