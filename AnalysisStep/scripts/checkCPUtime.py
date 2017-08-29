c1fbc2c2dd5649341c7853ce4e7e9cf3f53522d4
Heshy Roskes <jroskes1@jhu.edu>
2017-08-29 16:30:28 +0200

    safer and document

--- AnalysisStep/scripts/checkCPUtime.py
+++ AnalysisStep/scripts/checkCPUtime.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
+"""
+Check folders that exceeded the CPU time limit.
+If it looks like everything is there, edit the
+output file to say that they didn't exceed the
+limit.  This can happen if LSF tries to kill the
+job but doesn't manage to do it until the job
+finishes.
+
+Because there might be corner cases, and it's
+extra important to have all the data, this script
+doesn't run on data unless the --force-data flag
+is used.
+"""
+
 import argparse, glob, gzip, os, re, sys
 import ROOT
 
-def checkCPUtime(folder):
+def checkCPUtime(folder, forcedata=False):
   if not os.path.isdir(folder):
     raise IOError("folder {} does not exist")
 
@@ -14,6 +28,17 @@ def checkCPUtime(folder):
         return "exited with exit status "+contents.strip()
   except IOError:
     return "no exitStatus.txt"
+
+  try:
+    with open(os.path.join(folder, "run_cfg.py")) as f:
+      matches = set()
+      for line in f:
+        match = re.search(r"""\bPD = cms.string[(]["'](.*)["'][)]""")
+        if match: matches.add(match.group(1))
+      if matches != {""} and not forcedata:
+        return "this folder is data, so we want to be extra careful with failed jobs.  If you're sure, you can run with --force-data."
+  except IOError:
+    return "no run_cfg.py"
 
   jobouts = sorted(glob.glob(os.path.join(folder, "job_*.txt")))
   if not jobouts: return
@@ -55,14 +80,15 @@ def checkCPUtime(folder):
   return "CPU time limit is reported as exceeded, but looks like everything is there :).  Marking it as [NOT] exceeded."
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Check folders that exceeded the CPU time limit.')
+  parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument('folders', metavar='folder', nargs='*', help='folders to check')
-  parser.add_argument('--quiet', '-q', action='store_true')
+  parser.add_argument('--quiet', '-q', action='store_true', help="don't print anything")
+  parser.add_argument('--force-data', action='store_true', help="run even on data folders (dangerous!)")
   args = parser.parse_args()
 
   if not args.folders: args.folders = glob.glob("*Chunk*/")
 
   for folder in args.folders:
-    result = checkCPUtime(folder)
+    result = checkCPUtime(folder, forcedata=args.force_data)
     if not args.quiet:
       print "check CPU time for {}: {}".format(folder, result)
