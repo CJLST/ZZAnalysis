391318287d4bed797a7b4e96ac2dcfc7165c0afb
Heshy Roskes <jroskes1@jhu.edu>
2018-02-05 18:40:34 +0100

    store POWHEG original weight, better normalization for systematics

--- AnalysisStep/test/Ntuplizers/HZZ4lNtupleMaker.cc
+++ AnalysisStep/test/Ntuplizers/HZZ4lNtupleMaker.cc
@@ -246,6 +246,7 @@ namespace {
   std::vector<short> LHEAssociatedParticleId;
 
   Float_t LHEPDFScale = 0;
+  Float_t LHEweight_POWHEGonly = 0;
   Float_t LHEweight_QCDscale_muR1_muF1  = 0;
   Float_t LHEweight_QCDscale_muR1_muF2  = 0;
   Float_t LHEweight_QCDscale_muR1_muF0p5  = 0;
@@ -1251,6 +1252,7 @@ void HZZ4lNtupleMaker::FillLHECandidate(){
   LHEAssociatedParticleId.clear();
 
   LHEPDFScale = 0;
+  LHEweight_POWHEGonly = 0;
   LHEweight_QCDscale_muR1_muF1=0;
   LHEweight_QCDscale_muR1_muF2=0;
   LHEweight_QCDscale_muR1_muF0p5=0;
@@ -1359,6 +1361,7 @@ void HZZ4lNtupleMaker::FillLHECandidate(){
     genHEPMCweight *= lheHandler->reweightNNLOtoNLO();
   }
 
+  LHEweight_POWHEGonly = lheHandler->getPowhegOriginalWeight();
   LHEweight_QCDscale_muR1_muF1 = lheHandler->getLHEWeight(0, 1.);
   LHEweight_QCDscale_muR1_muF2 = lheHandler->getLHEWeight(1, 1.);
   LHEweight_QCDscale_muR1_muF0p5 = lheHandler->getLHEWeight(2, 1.);
@@ -2282,6 +2285,7 @@ void HZZ4lNtupleMaker::BookAllBranches(){
     }
 
     myTree->Book("LHEPDFScale", LHEPDFScale, failedTreeLevel >= minimalFailedTree);
+    myTree->Book("LHEweight_POWHEGonly", LHEweight_POWHEGonly, failedTreeLevel >= minimalFailedTree);
     myTree->Book("LHEweight_QCDscale_muR1_muF1", LHEweight_QCDscale_muR1_muF1, failedTreeLevel >= minimalFailedTree);
     myTree->Book("LHEweight_QCDscale_muR1_muF2", LHEweight_QCDscale_muR1_muF2, failedTreeLevel >= minimalFailedTree);
     myTree->Book("LHEweight_QCDscale_muR1_muF0p5", LHEweight_QCDscale_muR1_muF0p5, failedTreeLevel >= minimalFailedTree);
