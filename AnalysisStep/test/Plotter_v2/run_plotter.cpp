23b0fc211e54d595473a68ec613e2772cdcbc947
Toni Sculac <toni.sculac@cern.ch>
2019-02-14 11:08:28 +0100

    Update Plotter_v2

--- AnalysisStep/test/Plotter_v2/run_plotter.cpp
+++ AnalysisStep/test/Plotter_v2/run_plotter.cpp
@@ -26,9 +26,10 @@ int main( int argc, char *argv[] )
    
    TString path = "";
    TString file_name = "/ZZ4lAnalysis.root";
-   TString file_name_FR = "/FakeRates_SS_Moriond18.root";
+   TString file_name_FR = "/FakeRates_SS_Moriond19.root";
 	
-   TString Data    = "AllData"        + file_name;
+
+   TString Data        = path + "AllData"    + file_name;
 	TString FakeRates   = "../../data/FakeRates" + file_name_FR;
 	
    TString ggH125      = path + "ggH125"     + file_name;
@@ -40,7 +41,7 @@ int main( int argc, char *argv[] )
    TString bbH125      = path + "bbH125"     + file_name;
    TString tqH125      = path + "tqH125"     + file_name;
 	
-   TString ZZTo4l      = path + "ZZTo4lext"                     + file_name;
+   TString ZZTo4l      = path + "ZZTo4lext1"                 + file_name;
    TString ggZZ4e      = path + "ggTo4e_Contin_MCFM701"      + file_name;
    TString ggZZ4mu     = path + "ggTo4mu_Contin_MCFM701"     + file_name;
    TString ggZZ4tau    = path + "ggTo4tau_Contin_MCFM701"    + file_name;
@@ -48,9 +49,9 @@ int main( int argc, char *argv[] )
    TString ggZZ2e2tau  = path + "ggTo2e2tau_Contin_MCFM701"  + file_name;
    TString ggZZ2mu2tau = path + "ggTo2mu2tau_Contin_MCFM701" + file_name;
 
-   Plotter *plotter = new Plotter( 41.53 );
+   Plotter *plotter = new Plotter( 58.83 );
 
-   plotter->SetBlinding(110, 150, 300, 1200);
+   plotter->SetBlinding(110, 138, 300, 1200);
 
    plotter->MakeHistograms(Data);
    plotter->MakeHistograms(ggH125);
@@ -81,54 +82,54 @@ int main( int argc, char *argv[] )
 //===========================
 // Plotting of blinded plots
 //===========================
-//   plotter->GetHistos( "Blinded" );
-//
-//   plotter->plot_1D_all_cat("Blinded", "M4lMain",       "Plots/Blinded");
-//   plotter->plot_1D_all_cat("Blinded", "M4lMainZoomed", "Plots/Blinded");
-//
-//   plotter->plot_1D_all_fs("Blinded", "M4lMain",       "Plots/Blinded");
-//   plotter->plot_1D_all_fs("Blinded", "M4lMainZoomed", "Plots/Blinded");
-//
-//   plotter->plot_1D_single("Blinded", "M4lMainHighMass", "Plots/Blinded", Settings::fs4l, Settings::inclusive);
-//   plotter->plot_1D_single("Blinded", "MZ1",             "Plots/Blinded", Settings::fs4l, Settings::inclusive);
-//   plotter->plot_1D_single("Blinded", "MZ2",             "Plots/Blinded", Settings::fs4l, Settings::inclusive);
-//	  plotter->plot_1D_single("Blinded", "MZ1",             "Plots/Blinded", Settings::fs4e, Settings::inclusive);
-//   plotter->plot_1D_single("Blinded", "MZ2",             "Plots/Blinded", Settings::fs4e, Settings::inclusive);
-//	  plotter->plot_1D_single("Blinded", "MZ1",             "Plots/Blinded", Settings::fs4mu, Settings::inclusive);
-//   plotter->plot_1D_single("Blinded", "MZ2",             "Plots/Blinded", Settings::fs4mu, Settings::inclusive);
-//	  plotter->plot_1D_single("Blinded", "MZ1",             "Plots/Blinded", Settings::fs2e2mu, Settings::inclusive);
-//   plotter->plot_1D_single("Blinded", "MZ2",             "Plots/Blinded", Settings::fs2e2mu, Settings::inclusive);
-//
-//   plotter->plot_1D_single("Blinded", "KD", "Plots/Blinded", Settings::fs4l, Settings::inclusive);
-//   plotter->plot_1D_single("Blinded", "KD", "Plots/Blinded", Settings::fs4e, Settings::inclusive);
-//   plotter->plot_1D_single("Blinded", "KD", "Plots/Blinded", Settings::fs4mu, Settings::inclusive);
-//   plotter->plot_1D_single("Blinded", "KD", "Plots/Blinded", Settings::fs2e2mu, Settings::inclusive);
-//
-//   plotter->plot_1D_single("Blinded", "DVBFDEC", "Plots/Blinded", Settings::fs4l, Settings::VBF_2j_tagged);
-//   plotter->plot_1D_single("Blinded", "DVHDEC",  "Plots/Blinded", Settings::fs4l, Settings::VH_hadron_tagged);
-//
-//	  plotter->plot_1D_single("Blinded", "D1jet", "Plots/Blinded", Settings::fs4l, Settings::inclusive);
-//	  plotter->plot_1D_single("Blinded", "D2jet", "Plots/Blinded", Settings::fs4l, Settings::inclusive);
-//	  plotter->plot_1D_single("Blinded", "DVH",   "Plots/Blinded", Settings::fs4l, Settings::inclusive);
-//
-//   plotter->plot_2D_single("Blinded", "MZ1vsMZ2", "Plots/Blinded", Settings::inclusive);
-//
-//   plotter->plot_2D_error_single("Blinded", "KDvsM4l",          "Plots/Blinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Blinded", "KDvsM4lZoomed",    "Plots/Blinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Blinded", "KDvsM4lHighMass",  "Plots/Blinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Blinded", "D1jetvsM4lZoomed", "Plots/Blinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Blinded", "D2jetvsM4lZoomed", "Plots/Blinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Blinded", "DWHvsM4lZoomed",   "Plots/Blinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Blinded", "DZHvsM4lZoomed",   "Plots/Blinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Blinded", "DVHvsM4lZoomed",   "Plots/Blinded", Settings::inclusive);
-//
+   plotter->GetHistos( "Blinded" );
+
+   plotter->plot_1D_all_cat("Blinded", "M4lMain",       "Plots/Blinded");
+   plotter->plot_1D_all_cat("Blinded", "M4lMainZoomed", "Plots/Blinded");
+
+   plotter->plot_1D_all_fs("Blinded", "M4lMain",       "Plots/Blinded");
+   plotter->plot_1D_all_fs("Blinded", "M4lMainZoomed", "Plots/Blinded");
+
+   plotter->plot_1D_single("Blinded", "M4lMainHighMass", "Plots/Blinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "MZ1",             "Plots/Blinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "MZ2",             "Plots/Blinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "MZ1",             "Plots/Blinded", Settings::fs4e, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "MZ2",             "Plots/Blinded", Settings::fs4e, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "MZ1",             "Plots/Blinded", Settings::fs4mu, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "MZ2",             "Plots/Blinded", Settings::fs4mu, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "MZ1",             "Plots/Blinded", Settings::fs2e2mu, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "MZ2",             "Plots/Blinded", Settings::fs2e2mu, Settings::inclusive);
+
+   plotter->plot_1D_single("Blinded", "KD", "Plots/Blinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "KD", "Plots/Blinded", Settings::fs4e, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "KD", "Plots/Blinded", Settings::fs4mu, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "KD", "Plots/Blinded", Settings::fs2e2mu, Settings::inclusive);
+
+   plotter->plot_1D_single("Blinded", "DVBFDEC", "Plots/Blinded", Settings::fs4l, Settings::VBF_2j_tagged);
+   plotter->plot_1D_single("Blinded", "DVHDEC",  "Plots/Blinded", Settings::fs4l, Settings::VH_hadron_tagged);
+
+   plotter->plot_1D_single("Blinded", "D1jet", "Plots/Blinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "D2jet", "Plots/Blinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Blinded", "DVH",   "Plots/Blinded", Settings::fs4l, Settings::inclusive);
+
+   plotter->plot_2D_single("Blinded", "MZ1vsMZ2", "Plots/Blinded", Settings::inclusive);
+
+   plotter->plot_2D_error_single("Blinded", "KDvsM4l",          "Plots/Blinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Blinded", "KDvsM4lZoomed",    "Plots/Blinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Blinded", "KDvsM4lHighMass",  "Plots/Blinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Blinded", "D1jetvsM4lZoomed", "Plots/Blinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Blinded", "D2jetvsM4lZoomed", "Plots/Blinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Blinded", "DWHvsM4lZoomed",   "Plots/Blinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Blinded", "DZHvsM4lZoomed",   "Plots/Blinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Blinded", "DVHvsM4lZoomed",   "Plots/Blinded", Settings::inclusive);
+
 //   plotter->plot_2D_error_all_cat("Blinded", "KDvsM4lZoomed",    "Plots/Blinded");
 //   plotter->plot_2D_error_all_cat("Blinded", "D1jetvsM4lZoomed", "Plots/Blinded");
 //   plotter->plot_2D_error_all_cat("Blinded", "D2jetvsM4lZoomed", "Plots/Blinded");
 //   plotter->plot_2D_error_all_cat("Blinded", "DWHvsM4lZoomed",   "Plots/Blinded");
 //   plotter->plot_2D_error_all_cat("Blinded", "DZHvsM4lZoomed",   "Plots/Blinded");
 //   plotter->plot_2D_error_all_cat("Blinded", "DVHvsM4lZoomed",   "Plots/Blinded");
-//
+
 	
    
 //=============================
@@ -139,49 +140,49 @@ int main( int argc, char *argv[] )
 
    plotter->GetHistos("Unblinded");
 	
-//   plotter->plot_1D_all_cat("Unblinded", "M4lMain",       "Plots/Unblinded");
-//
-//   plotter->plot_1D_all_fs("Unblinded", "M4lMain",       "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "M4lMainZoomed", "Plots/Unblinded");
-//
-//   plotter->plot_1D_all_fs("Unblinded", "PFMET",             "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "Pt4l",              "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "Eta4l",             "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "Pt_leading",        "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "Pt_trailing",       "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "SIP_leading",       "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "SIP_trailing",      "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "ISO_leading",       "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "ISO_trailing",      "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "NExtraLep",         "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "NJets",             "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "NJetsBTagged",      "Plots/Unblinded");
-//   plotter->plot_1D_all_fs("Unblinded", "M4l_110150_HighKD", "Plots/Unblinded");
-//   plotter->plot_1D_all_cat("Unblinded", "M4l_110150_HighKD", "Plots/Unblinded");
-//
-//   plotter->plot_1D_single("Unblinded", "M4lMainHighMass", "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-//   plotter->plot_1D_single("Unblinded", "MZ1",             "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-//   plotter->plot_1D_single("Unblinded", "MZ2",             "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-//
-//
-//   plotter->plot_1D_single("Unblinded", "M4l_110150_HighKD", "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-//   plotter->plot_1D_single("Unblinded", "KD",               "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-//   plotter->plot_1D_single("Unblinded", "DVBFDEC",          "Plots/Unblinded", Settings::fs4l, Settings::VBF_2j_tagged);
-//   plotter->plot_1D_single("Unblinded", "DVHDEC",           "Plots/Unblinded", Settings::fs4l, Settings::VH_hadron_tagged);
-//   plotter->plot_1D_single("Unblinded", "DWH_M4L118130",    "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-//   plotter->plot_1D_single("Unblinded", "DZH_M4L118130",    "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-//
-//   plotter->plot_2D_single("Unblinded", "MZ1vsMZ2",           "Plots/Unblinded", Settings::inclusive);
-//
-//   plotter->plot_2D_error_single("Unblinded", "KDvsM4l",          "Plots/Unblinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Unblinded", "KDvsM4lZoomed",    "Plots/Unblinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Unblinded", "KDvsM4lHighMass",  "Plots/Unblinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Unblinded", "D1jetvsM4lZoomed", "Plots/Unblinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Unblinded", "D2jetvsM4lZoomed", "Plots/Unblinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Unblinded", "DWHvsM4lZoomed",   "Plots/Unblinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Unblinded", "DZHvsM4lZoomed",   "Plots/Unblinded", Settings::inclusive);
-//   plotter->plot_2D_error_single("Unblinded", "DVHvsM4lZoomed",   "Plots/Unblinded", Settings::inclusive);
-//
+   plotter->plot_1D_all_cat("Unblinded", "M4lMain",       "Plots/Unblinded");
+
+   plotter->plot_1D_all_fs("Unblinded", "M4lMain",       "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "M4lMainZoomed", "Plots/Unblinded");
+
+   plotter->plot_1D_all_fs("Unblinded", "PFMET",             "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "Pt4l",              "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "Eta4l",             "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "Pt_leading",        "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "Pt_trailing",       "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "SIP_leading",       "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "SIP_trailing",      "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "ISO_leading",       "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "ISO_trailing",      "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "NExtraLep",         "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "NJets",             "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "NJetsBTagged",      "Plots/Unblinded");
+   plotter->plot_1D_all_fs("Unblinded", "M4l_110150_HighKD", "Plots/Unblinded");
+   plotter->plot_1D_all_cat("Unblinded", "M4l_110150_HighKD", "Plots/Unblinded");
+
+   plotter->plot_1D_single("Unblinded", "M4lMainHighMass", "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Unblinded", "MZ1",             "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Unblinded", "MZ2",             "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+
+
+   plotter->plot_1D_single("Unblinded", "M4l_110150_HighKD", "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Unblinded", "KD",               "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Unblinded", "DVBFDEC",          "Plots/Unblinded", Settings::fs4l, Settings::VBF_2j_tagged);
+   plotter->plot_1D_single("Unblinded", "DVHDEC",           "Plots/Unblinded", Settings::fs4l, Settings::VH_hadron_tagged);
+   plotter->plot_1D_single("Unblinded", "DWH_M4L118130",    "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+   plotter->plot_1D_single("Unblinded", "DZH_M4L118130",    "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+
+   plotter->plot_2D_single("Unblinded", "MZ1vsMZ2",           "Plots/Unblinded", Settings::inclusive);
+
+   plotter->plot_2D_error_single("Unblinded", "KDvsM4l",          "Plots/Unblinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Unblinded", "KDvsM4lZoomed",    "Plots/Unblinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Unblinded", "KDvsM4lHighMass",  "Plots/Unblinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Unblinded", "D1jetvsM4lZoomed", "Plots/Unblinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Unblinded", "D2jetvsM4lZoomed", "Plots/Unblinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Unblinded", "DWHvsM4lZoomed",   "Plots/Unblinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Unblinded", "DZHvsM4lZoomed",   "Plots/Unblinded", Settings::inclusive);
+   plotter->plot_2D_error_single("Unblinded", "DVHvsM4lZoomed",   "Plots/Unblinded", Settings::inclusive);
+
 //   plotter->plot_2D_error_all_cat("Unblinded", "D1jetvsM4lZoomed", "Plots/Unblinded");
 //   plotter->plot_2D_error_all_cat("Unblinded", "D2jetvsM4lZoomed", "Plots/Unblinded");
 //   plotter->plot_2D_error_all_cat("Unblinded", "DWHvsM4lZoomed",   "Plots/Unblinded");
@@ -191,22 +192,22 @@ int main( int argc, char *argv[] )
 //=============================
 // PAS HIG-18-001 figures
 //=============================
-
-   plotter->plot_1D_single("Unblinded", "M4lMain", "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-   plotter->plot_1D_single("Unblinded", "M4lMainZoomed", "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-	plotter->plot_1D_all_cat("Unblinded", "M4lMainZoomed", "Plots/Unblinded");
-   plotter->plot_1D_single("Unblinded", "MZ1_M4L118130",   "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-   plotter->plot_1D_single("Unblinded", "MZ2_M4L118130",   "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-   plotter->plot_1D_single("Unblinded", "KD_M4L118130",     "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-   plotter->plot_1D_single("Unblinded", "DVBFDEC_M4L118130","Plots/Unblinded", Settings::fs4l, Settings::VBF_2j_tagged);
-   plotter->plot_1D_single("Unblinded", "DVHDEC_M4L118130", "Plots/Unblinded", Settings::fs4l, Settings::VH_hadron_tagged);
-   plotter->plot_1D_single("Unblinded", "D1jet_M4L118130",  "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-   plotter->plot_1D_single("Unblinded", "D2jet_M4L118130",  "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-	plotter->plot_1D_single("Unblinded", "DVH_M4L118130",    "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
-	plotter->plot_2D_single("Unblinded", "MZ1vsMZ2_M4L118130", "Plots/Unblinded", Settings::inclusive);
-	plotter->plot_2D_error_all_cat("Unblinded", "KDvsM4lZoomed",    "Plots/Unblinded");
-   plotter->plot_2D_error_all_cat("Unblinded", "DVBFDECvsM4lZoomed",    "Plots/Unblinded");
-   plotter->plot_2D_error_all_cat("Unblinded", "DVHDECvsM4lZoomed",    "Plots/Unblinded");
+//
+//   plotter->plot_1D_single("Unblinded", "M4lMain", "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+//   plotter->plot_1D_single("Unblinded", "M4lMainZoomed", "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+//   plotter->plot_1D_all_cat("Unblinded", "M4lMainZoomed", "Plots/Unblinded");
+//   plotter->plot_1D_single("Unblinded", "MZ1_M4L118130",   "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+//   plotter->plot_1D_single("Unblinded", "MZ2_M4L118130",   "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+//   plotter->plot_1D_single("Unblinded", "KD_M4L118130",     "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+//   plotter->plot_1D_single("Unblinded", "DVBFDEC_M4L118130","Plots/Unblinded", Settings::fs4l, Settings::VBF_2j_tagged);
+//   plotter->plot_1D_single("Unblinded", "DVHDEC_M4L118130", "Plots/Unblinded", Settings::fs4l, Settings::VH_hadron_tagged);
+//   plotter->plot_1D_single("Unblinded", "D1jet_M4L118130",  "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+//   plotter->plot_1D_single("Unblinded", "D2jet_M4L118130",  "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+//   plotter->plot_1D_single("Unblinded", "DVH_M4L118130",    "Plots/Unblinded", Settings::fs4l, Settings::inclusive);
+//   plotter->plot_2D_single("Unblinded", "MZ1vsMZ2_M4L118130", "Plots/Unblinded", Settings::inclusive);
+//   plotter->plot_2D_error_all_cat("Unblinded", "KDvsM4lZoomed",    "Plots/Unblinded");
+//   plotter->plot_2D_error_all_cat("Unblinded", "DVBFDECvsM4lZoomed",    "Plots/Unblinded");
+//   plotter->plot_2D_error_all_cat("Unblinded", "DVHDECvsM4lZoomed",    "Plots/Unblinded");
 	
    delete plotter;
 }
