7c8037becbe00bfd34c9640e47fc21a8e484cfee
Toni Sculac <toni.sculac@cern.ch>
2019-03-27 13:25:16 +0100

    Update json files for pilup calculation.

--- AnalysisStep/test/prod/convertJSON.py
+++ AnalysisStep/test/prod/convertJSON.py
@@ -5,7 +5,7 @@
 
 #filename = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt' #for 2016 taken from https://twiki.cern.ch/twiki/bin/view/CMS/PdmV2016Analysis#DATA
 #filename = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt' #for 2017 taken from https://twiki.cern.ch/twiki/bin/view/CMS/PdmV2017Analysis#13_TeV_pp_runs_ReReco
-filename = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt' #for 2018 taken from https://twiki.cern.ch/twiki/bin/view/CMS/PdmV2018Analysis#Early2018Re_reco_17Sep2018_datas
+#filename = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt' #for 2018 taken from https://twiki.cern.ch/twiki/bin/view/CMS/PdmV2018Analysis#Early2018Re_reco_17Sep2018_datas
 
 json = eval( open(filename).read() )
 
