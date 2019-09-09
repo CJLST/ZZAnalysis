5b7a774b43291a43fda26b39f8dac61b33c8233d
Toni Sculac <toni.sculac@cern.ch>
2019-09-09 10:14:21 +0200

    Fix Plotter for compiling.

--- AnalysisStep/test/Plotter_v2/run_combination.cpp
+++ AnalysisStep/test/Plotter_v2/run_combination.cpp
@@ -25,18 +25,20 @@ int main( int argc, char *argv[] )
 {
    setTDRStyle();
 	
-   TString path_2018 = "";
-   TString path_2017 = "";
-   TString path_2016 = "";
+   TString eos_path  = "";
+   
+   TString path_2018 = eos_path + "MC_2018/";
+   TString path_2017 = eos_path + "MC_2017/";
+   TString path_2016 = eos_path + "MC_2016/";
    TString file_name = "/ZZ4lAnalysis.root";
-	
-   TString Data_2018    = path_2018 + "AllData"        + file_name;
-   TString Data_2017    = path_2017 + "AllData"        + file_name;
-   TString Data_2016    = path_2016 + "AllData"        + file_name;
-    
-   TString FR_2018      = "../../data/FakeRates/FakeRates_SS_Moriond19.root";
-   TString FR_2017      = "../../data/FakeRates/FakeRates_SS_Moriond18.root";
-   TString FR_2016      = "../../data/FakeRates/FakeRate_SS_Moriond368.root";
+   
+   TString Data_2018    = eos_path + "Data_2018/AllData"        + file_name;
+   TString Data_2017    = eos_path + "Data_2017/AllData"        + file_name;
+   TString Data_2016    = eos_path + "Data_2016/AllData"        + file_name;
+   
+   TString FR_2018      = "../../data/FakeRates/FakeRates_SS_2018_Legacy.root";
+   TString FR_2017      = "../../data/FakeRates/FakeRates_SS_2017_Legacy.root";
+   TString FR_2016      = "../../data/FakeRates/FakeRates_SS_2016_Legacy.root";
    
    TString ggH125_2018      = path_2018 + "ggH125"     + file_name;
    TString VBFH125_2018     = path_2018 + "VBFH125"    + file_name;
@@ -47,14 +49,14 @@ int main( int argc, char *argv[] )
    TString bbH125_2018      = path_2018 + "bbH125"     + file_name;
    TString tqH125_2018      = path_2018 + "tqH125"     + file_name;
    
-   TString ZZTo4l_2018      = path_2018 + "ZZTo4lext2"                 + file_name;
+   TString ZZTo4l_2018      = path_2018 + "ZZTo4lext1"                 + file_name;
    TString ggZZ4e_2018      = path_2018 + "ggTo4e_Contin_MCFM701"      + file_name;
    TString ggZZ4mu_2018     = path_2018 + "ggTo4mu_Contin_MCFM701"     + file_name;
    TString ggZZ4tau_2018    = path_2018 + "ggTo4tau_Contin_MCFM701"    + file_name;
    TString ggZZ2e2mu_2018   = path_2018 + "ggTo2e2mu_Contin_MCFM701"   + file_name;
    TString ggZZ2e2tau_2018  = path_2018 + "ggTo2e2tau_Contin_MCFM701"  + file_name;
    TString ggZZ2mu2tau_2018 = path_2018 + "ggTo2mu2tau_Contin_MCFM701" + file_name;
-	
+   
    TString ggH125_2017      = path_2017 + "ggH125"     + file_name;
    TString VBFH125_2017     = path_2017 + "VBFH125"    + file_name;
    TString WpH125_2017      = path_2017 + "WplusH125"  + file_name;
@@ -63,15 +65,15 @@ int main( int argc, char *argv[] )
    TString ttH125_2017      = path_2017 + "ttH125"     + file_name;
    TString bbH125_2017      = path_2017 + "bbH125"     + file_name;
    TString tqH125_2017      = path_2017 + "tqH125"     + file_name;
-	
-   TString ZZTo4l_2017      = path_2017 + "ZZTo4lext"                     + file_name;
+   
+   TString ZZTo4l_2017      = path_2017 + "ZZTo4l"                     + file_name;
    TString ggZZ4e_2017      = path_2017 + "ggTo4e_Contin_MCFM701"      + file_name;
    TString ggZZ4mu_2017     = path_2017 + "ggTo4mu_Contin_MCFM701"     + file_name;
    TString ggZZ4tau_2017    = path_2017 + "ggTo4tau_Contin_MCFM701"    + file_name;
    TString ggZZ2e2mu_2017   = path_2017 + "ggTo2e2mu_Contin_MCFM701"   + file_name;
    TString ggZZ2e2tau_2017  = path_2017 + "ggTo2e2tau_Contin_MCFM701"  + file_name;
    TString ggZZ2mu2tau_2017 = path_2017 + "ggTo2mu2tau_Contin_MCFM701" + file_name;
-	
+   
    TString ggH125_2016      = path_2016 + "ggH125"     + file_name;
    TString VBFH125_2016     = path_2016 + "VBFH125"    + file_name;
    TString WpH125_2016      = path_2016 + "WplusH125"  + file_name;
@@ -80,8 +82,8 @@ int main( int argc, char *argv[] )
    TString ttH125_2016      = path_2016 + "ttH125"     + file_name;
    TString bbH125_2016      = path_2016 + "bbH125"     + file_name;
    TString tqH125_2016      = path_2016 + "tqH125"     + file_name;
-
-   TString ZZTo4l_2016      = path_2016 + "ZZTo4lext"                     + file_name;
+   
+   TString ZZTo4l_2016      = path_2016 + "ZZTo4l"                     + file_name;
    TString ggZZ4e_2016      = path_2016 + "ggTo4e_Contin_MCFM701"      + file_name;
    TString ggZZ4mu_2016     = path_2016 + "ggTo4mu_Contin_MCFM701"     + file_name;
    TString ggZZ4tau_2016    = path_2016 + "ggTo4tau_Contin_MCFM701"    + file_name;
@@ -106,7 +108,7 @@ int main( int argc, char *argv[] )
    combination->FillHistograms(ggZZ4tau_2018,2018);
    combination->FillHistograms(ggZZ2e2mu_2018,2018);
    combination->FillHistograms(ggZZ2e2tau_2018,2018);
-   combination->FillHistograms(ggZZ2mu2tau_2018,2018);
+   //combination->FillHistograms(ggZZ2mu2tau_2018,2018);
    combination->FillZXHistograms(Data_2018, FR_2018, 2018);
 
    combination->FillHistograms(Data_2017,2017);
@@ -152,7 +154,10 @@ int main( int argc, char *argv[] )
    
    combination->GetComb();
 
-   combination->PlotM4l();setTDRStyle();
+//   combination->PlotM4l(Settings::fs4e);setTDRStyle();
+//   combination->PlotM4l(Settings::fs4mu);setTDRStyle();
+//   combination->PlotM4l(Settings::fs2e2mu);setTDRStyle();
+//   combination->PlotM4l(Settings::fs4l);setTDRStyle();
    combination->PlotSTXS();setTDRStyle();
    combination->PlotPurity();setTDRStyle();
 	
