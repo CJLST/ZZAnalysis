dfce25c6d316ae9cb905f9414c9b7202c0f5a98e
Toni Sculac <toni.sculac@cern.ch>
2019-09-12 10:06:34 +0200

    Update Z+X with SIP<4 cut.

--- AnalysisStep/test/Plotter_v2/include/ZXVariables.h
+++ AnalysisStep/test/Plotter_v2/include/ZXVariables.h
@@ -16,11 +16,11 @@ public:
    struct ZX4e
    {
       // 2016 Z+X Yields
-      float yield_SS_4e_2016      = 13.0;
-      float yield_Comb_4e_2016    = 15.9;
+      float yield_SS_4e_2016      = 13.1;
+      float yield_Comb_4e_2016    = 16.2;
       // 2017 Z+X Yields
       float yield_SS_4e_2017      = 10.8;
-      float yield_Comb_4e_2017    = 12.7;
+      float yield_Comb_4e_2017    = 13.0;
       // 2018 Z+X Yields
       float yield_SS_4e_2018      = 16.2;
       float yield_Comb_4e_2018    = 19.6;
@@ -38,14 +38,14 @@ public:
     struct ZX4mu
     {
       // 2016 Z+X Yields
-      float yield_SS_4mu_2016     = 13.6;
-      float yield_Comb_4mu_2016   = 15.1;
+      float yield_SS_4mu_2016     = 26.5;
+      float yield_Comb_4mu_2016   = 26.1;
       // 2017 Z+X Yields
-      float yield_SS_4mu_2017     = 17.5;
-      float yield_Comb_4mu_2017   = 20.1;
+      float yield_SS_4mu_2017     = 30.0;
+      float yield_Comb_4mu_2017   = 30.9;
       // 2018 Z+X Yields
-      float yield_SS_4mu_2018     = 33.0;
-      float yield_Comb_4mu_2018   = 36.1;
+      float yield_SS_4mu_2018     = 49.9;
+      float yield_Comb_4mu_2018   = 48.8;
        
       float norm_2016         = yield_Comb_4mu_2016/yield_SS_4mu_2016;
       float norm_2017         = yield_Comb_4mu_2017/yield_SS_4mu_2017;
@@ -60,14 +60,14 @@ public:
     struct ZX2e2mu
     {
       // 2016 Z+X Yields
-      float yield_SS_2e2mu_2016   = 27.9;
-      float yield_Comb_2e2mu_2016 = 33.6;
+      float yield_SS_2e2mu_2016   = 38.5;
+      float yield_Comb_2e2mu_2016 = 42.5;
       // 2017 Z+X Yields
-      float yield_SS_2e2mu_2017   = 28.2;
-      float yield_Comb_2e2mu_2017 = 32.2;
+      float yield_SS_2e2mu_2017   = 37.9;
+      float yield_Comb_2e2mu_2017 = 40.4;
       // 2018 Z+X Yields
-      float yield_SS_2e2mu_2018   = 48.0;
-      float yield_Comb_2e2mu_2018 = 53.8;
+      float yield_SS_2e2mu_2018   = 59.5;
+      float yield_Comb_2e2mu_2018 = 63.1;
       
       float norm_2016         = yield_Comb_2e2mu_2016/yield_SS_2e2mu_2016;
       float norm_2017         = yield_Comb_2e2mu_2017/yield_SS_2e2mu_2017;
