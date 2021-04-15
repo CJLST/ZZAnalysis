9e2087fe8c35c807db4e3e1034860e076fc63391
asculac <73283250+asculac@users.noreply.github.com>
2021-04-15 12:00:18 +0200

    Updated EleFiller.cc for 17UL and 16UL

--- AnalysisStep/plugins/EleFiller.cc
+++ AnalysisStep/plugins/EleFiller.cc
@@ -121,10 +121,10 @@ EleFiller::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 	  
     // Load correct RunII BDT ID+iso
     float BDT = -99;
-    if      ( setup == 2016 ) BDT = l.userFloat("ElectronMVAEstimatorRun2Summer16IdIsoValues");
-    else if ( setup == 2017 ) BDT = l.userFloat("ElectronMVAEstimatorRun2Fall17IsoV2Values");
+    if      ( setup == 2016 ) BDT = l.userFloat("ElectronMVAEstimatorRun2Summer16ULIdIsoValues");
+    else if ( setup == 2017 ) BDT = l.userFloat("ElectronMVAEstimatorRun2Summer17ULIdIsoV2Values");
     else if ( setup == 2018 ) BDT = l.userFloat("ElectronMVAEstimatorRun2Summer18ULIdIsoValues");
-//    cout << "BDT = " << BDT << endl;
+    cout << "BDT = " << BDT << endl;
     
     float pt = l.pt();
 
@@ -133,23 +133,23 @@ EleFiller::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
     if ( setup==2016 )
     {
-       //WP taken from https://github.com/mkovac/cmssw/blob/Electron_XGBoost_MVA_2016_CMSSW_10_3_1/RecoEgamma/ElectronIdentification/python/Identification/mvaElectronID_Summer16_ID_ISO_cff.py#L27-L34 and transfered with https://github.com/cms-sw/cmssw/blob/CMSSW_9_4_X/RecoEgamma/EgammaTools/interface/MVAValueMapProducer.h#L145 so that they are between -1 and 1
-       isBDT         = (pt<=10 && ((fSCeta<0.8                  && BDT >  0.95034841889) ||
-                                   (fSCeta>=0.8 && fSCeta<1.479 && BDT >  0.94606270058) ||
-                                   (fSCeta>=1.479               && BDT >  0.93872558098)))
-                    || (pt>10  && ((fSCeta<0.8                  && BDT >  0.3782357877) ||
-                                   (fSCeta>=0.8 && fSCeta<1.479 && BDT >  0.35871320305) ||
-                                   (fSCeta>=1.479               && BDT >  -0.57451499543)));
+       //WP taken from https://github.com/asculac/cmssw/blob/Electron_XGBoost_MVA_16UL_17UL/RecoEgamma/ElectronIdentification/python/Identification/mvaElectronID_Summer16UL_ID_ISO_cff.py#L27-L34 and transfered with https://github.com/cms-sw/cmssw/blob/CMSSW_9_4_X/RecoEgamma/EgammaTools/interface/MVAValueMapProducer.h#L145 so that they are between -1 and 1
+       isBDT         = (pt<=10 && ((fSCeta<0.8                  && BDT >  0.9557993256) ||
+                                   (fSCeta>=0.8 && fSCeta<1.479 && BDT >  0.9475406570) ||
+                                   (fSCeta>=1.479               && BDT >  0.9285158721)))
+                    || (pt>10  && ((fSCeta<0.8                  && BDT >  0.3272075608) ||
+                                   (fSCeta>=0.8 && fSCeta<1.479 && BDT >  0.2468345995) ||
+                                   (fSCeta>=1.479               && BDT >  -0.5955762814)));
     }
 	 else if (setup==2017)
 	 {
-	   //WP taken from https://github.com/cms-sw/cmssw/blob/master/RecoEgamma/ElectronIdentification/python/Identification/mvaElectronID_Fall17_iso_V2_cff.py#L21 and transfered with https://github.com/cms-sw/cmssw/blob/CMSSW_9_4_X/RecoEgamma/EgammaTools/interface/MVAValueMapProducer.h#L145 so that they are between -1 and 1
-	 	 isBDT         = (pt<=10 && ((fSCeta<0.8                  && BDT >  0.85216885148) ||
-                                   (fSCeta>=0.8 && fSCeta<1.479 && BDT >  0.82684550976) ||
-                                   (fSCeta>=1.479               && BDT >  0.86937630022)))
-                    || (pt>10  && ((fSCeta<0.8                  && BDT >  0.98248928759) ||
-                                   (fSCeta>=0.8 && fSCeta<1.479 && BDT >  0.96919224579) ||
-                                   (fSCeta>=1.479               && BDT >  0.79349796445)));
+	   //WP taken from https://github.com/asculac/cmssw/blob/Electron_XGBoost_MVA_16UL_17UL/RecoEgamma/ElectronIdentification/python/Identification/mvaElectronID_Summer17UL_ID_ISO_cff.py#L27-L34 and transfered with https://github.com/cms-sw/cmssw/blob/CMSSW_9_4_X/RecoEgamma/EgammaTools/interface/MVAValueMapProducer.h#L145 so that they are between -1 and 1
+       isBDT         = (pt<=10 && ((fSCeta<0.8                  && BDT >  0.9128577458) ||
+                                   (fSCeta>=0.8 && fSCeta<1.479 && BDT >  0.9056792368) ||
+                                   (fSCeta>=1.479               && BDT >  0.9439440575)))
+                    || (pt>10  && ((fSCeta<0.8                  && BDT >  0.1559788054) ||
+                                   (fSCeta>=0.8 && fSCeta<1.479 && BDT >  0.0273863727) ||
+                                   (fSCeta>=1.479               && BDT >  -0.5532483665)));
 	 }
     else if ( setup==2018 )
     {
