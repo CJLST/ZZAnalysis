ff3b4eec470a11a323e9c836d052d1be219de37a
Alessandro Tarabini <alessandro.tarabini@gmail.com>
2022-02-26 13:36:53 +0100

    UL Muon SF

--- AnalysisStep/src/LeptonSFHelper.cc
+++ AnalysisStep/src/LeptonSFHelper.cc
@@ -5,13 +5,13 @@ using namespace std;
 
 LeptonSFHelper::LeptonSFHelper(bool preVFP)
 {
-   // 2016 preVFP Electrons 
+   // 2016 preVFP Electrons
    if(preVFP)
    {  //ID
       TString fipEleNotCracks_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ElectronSF_UL2016preVFP_nogap.root");
       root_file = TFile::Open(fipEleNotCracks_2016.Data(),"READ");
       h_Ele_notCracks_2016 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-      
+
       TString fipEleCracks_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ElectronSF_UL2016preVFP_gap.root");
       root_file = TFile::Open(fipEleCracks_2016.Data(),"READ");
       h_Ele_Cracks_2016 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
@@ -20,43 +20,43 @@ LeptonSFHelper::LeptonSFHelper(bool preVFP)
       TString fipEleReco_highPt_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/egammaEffi_ptAbove20.txt_EGM2D_UL2016preVFP.root");
       root_file = TFile::Open(fipEleReco_highPt_2016.Data(),"READ");
       h_Ele_Reco_highPT_2016 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-      
+
       TString fipEleReco_lowPt_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/egammaEffi_ptBelow20.txt_EGM2D_UL2016preVFP.root");
       root_file = TFile::Open(fipEleReco_lowPt_2016.Data(),"READ");
       h_Ele_Reco_lowPT_2016 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-      
+
    }
-   // 2016 postVFP Electrons                                           
+   // 2016 postVFP Electrons
    else
    {  //ID
       TString fipEleNotCracks_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ElectronSF_UL2016postVFP_nogap.root");
       root_file = TFile::Open(fipEleNotCracks_2016.Data(),"READ");
       h_Ele_notCracks_2016 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-      
+
       TString fipEleCracks_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ElectronSF_UL2016postVFP_gap.root");
       root_file = TFile::Open(fipEleCracks_2016.Data(),"READ");
       h_Ele_Cracks_2016 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-      
+
       //RECO
       TString fipEleReco_highPt_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/egammaEffi_ptAbove20.txt_EGM2D_UL2016postVFP.root");
       root_file = TFile::Open(fipEleReco_highPt_2016.Data(),"READ");
       h_Ele_Reco_highPT_2016 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-      
+
       TString fipEleReco_lowPt_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/egammaEffi_ptBelow20.txt_EGM2D_UL2016postVFP.root");
       root_file = TFile::Open(fipEleReco_lowPt_2016.Data(),"READ");
       h_Ele_Reco_lowPT_2016 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-      
+
    }
-                                       
 
-                                              
 
-   
+
+
+
    // 2017 Electrons
    TString fipEleNotCracks_2017 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ElectronSF_UL2017_nogap.root");
    root_file = TFile::Open(fipEleNotCracks_2017.Data(),"READ");
    h_Ele_notCracks_2017 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-   
+
    TString fipEleCracks_2017 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ElectronSF_UL2017_gap.root");
    root_file = TFile::Open(fipEleCracks_2017.Data(),"READ");
    h_Ele_Cracks_2017 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
@@ -64,17 +64,17 @@ LeptonSFHelper::LeptonSFHelper(bool preVFP)
    TString fipEleReco_highPt_2017 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/egammaEffi_ptAbove20.txt_EGM2D_UL2017.root");
    root_file = TFile::Open(fipEleReco_highPt_2017.Data(),"READ");
    h_Ele_Reco_highPT_2017 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-   
+
    TString fipEleReco_lowPt_2017 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/egammaEffi_ptBelow20.txt_EGM2D_UL2017.root");
    root_file = TFile::Open(fipEleReco_lowPt_2017.Data(),"READ");
    h_Ele_Reco_lowPT_2017 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-      
+
 
    // 2018 Electrons
    TString fipEleNotCracks_2018 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ElectronSF_UL2018_nogap.root");
    root_file = TFile::Open(fipEleNotCracks_2018.Data(),"READ");
    h_Ele_notCracks_2018 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-   
+
    TString fipEleCracks_2018 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ElectronSF_UL2018_gap.root");
    root_file = TFile::Open(fipEleCracks_2018.Data(),"READ");
    h_Ele_Cracks_2018 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
@@ -82,32 +82,32 @@ LeptonSFHelper::LeptonSFHelper(bool preVFP)
    TString fipEleReco_highPt_2018 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/egammaEffi_ptAbove20.txt_EGM2D_UL2018.root");
    root_file = TFile::Open(fipEleReco_highPt_2018.Data(),"READ");
    h_Ele_Reco_highPT_2018 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-   
+
    TString fipEleReco_lowPt_2018 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/egammaEffi_ptBelow20.txt_EGM2D_UL2018.root");
    root_file = TFile::Open(fipEleReco_lowPt_2018.Data(),"READ");
    h_Ele_Reco_lowPT_2018 = (TH2F*) root_file->Get("EGamma_SF2D")->Clone();
-   
+
    // 2016 Muons
-   TString fipMu_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/final_HZZ_muon_SF_2016RunB2H_legacy_newLoose_newIso_paper.root"); 
+   TString fipMu_2016 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/final_HZZ_SF_2016UL_mupogsysts_newLoose.root");
    //final_HZZ_SF_2016_legacy_mupogsysts.root"); //"$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ScaleFactors_mu_Moriond2017_v2.root");
    root_file = TFile::Open(fipMu_2016.Data(),"READ");
    h_Mu_SF_2016  = (TH2D*)root_file->Get("FINAL")->Clone();
    h_Mu_Unc_2016 = (TH2D*)root_file->Get("ERROR")->Clone();
-   
+
    // 2017 Muons
-   TString fipMu_2017 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/final_HZZ_muon_SF_2017_newLooseIso_mupogSysts_paper.root"); 
+   TString fipMu_2017 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/final_HZZ_SF_2017UL_mupogsysts_newLoose.root");
    //final_HZZ_SF_2017_rereco_mupogsysts_3010.root"); //"$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/ScaleFactors_mu_Moriond2018_final.root");
    root_file = TFile::Open(fipMu_2017.Data(),"READ");
    h_Mu_SF_2017  = (TH2D*)root_file->Get("FINAL")->Clone();
    h_Mu_Unc_2017 = (TH2D*)root_file->Get("ERROR")->Clone();
-      
+
    // 2018 Muons
-   TString fipMu_2018 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/final_HZZ_muon_SF_2018RunA2D_ER_newLoose_newIso_paper.root"); 
+   TString fipMu_2018 = Form("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/final_HZZ_SF_2018UL_mupogsysts_newLoose.root"); 
    //final_HZZ_SF_2018_rereco_mupogsysts_3010.root"); //"$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/LeptonEffScaleFactors/final_HZZ_muon_SF_2018RunA2D_ER_2702.root");
    root_file = TFile::Open(fipMu_2018.Data(),"READ");
    h_Mu_SF_2018  = (TH2D*)root_file->Get("FINAL")->Clone();
    h_Mu_Unc_2018 = (TH2D*)root_file->Get("ERROR")->Clone();
-   
+
    cout << "[LeptonSFHelper] SF maps opened from root files." << endl;
 }
 
@@ -120,9 +120,9 @@ float LeptonSFHelper::getSF(int year, int flav, float pt, float eta, float SCeta
    float RecoSF = 1.0;
    float SelSF = 1.0;
    float SF = 1.0;
-   
+
    //cout << "year = " << year << " flav = " << flav << " pt = " << pt << " eta = " << eta << " SCeta = " << SCeta << " isCrack = " << isCrack << endl;
-   
+
    // Electron reconstruction SFs
    if(abs(flav) == 11) {
       if(year == 2016)
@@ -162,7 +162,7 @@ float LeptonSFHelper::getSF(int year, int flav, float pt, float eta, float SCeta
          edm::LogError("LeptonSFHelper::") << "Ele SFs for " << year << " is not supported!";
          abort();
       }
-      
+
       // Electron HZZ selection SF
       if(year == 2016)
       {
@@ -174,9 +174,9 @@ float LeptonSFHelper::getSF(int year, int flav, float pt, float eta, float SCeta
             {
                SelSF = h_Ele_notCracks_2016->GetBinContent(h_Ele_notCracks_2016->FindFixBin(SCeta, std::min(pt,499.f)));
             }
-         
+
       }
-      
+
       else if(year == 2017)
       {
          if(isCrack)
@@ -188,7 +188,7 @@ float LeptonSFHelper::getSF(int year, int flav, float pt, float eta, float SCeta
                SelSF = h_Ele_notCracks_2017->GetBinContent(h_Ele_notCracks_2017->FindFixBin(SCeta, std::min(pt,499.f)));
             }
       }
-      
+
       else if(year == 2018)
       {
          if(isCrack)
@@ -200,16 +200,16 @@ float LeptonSFHelper::getSF(int year, int flav, float pt, float eta, float SCeta
                SelSF = h_Ele_notCracks_2018->GetBinContent(h_Ele_notCracks_2018->FindFixBin(SCeta, std::min(pt,499.f)));
             }
       }
-   
-      
+
+
    else {
       edm::LogError("LeptonSFHelper::") << "Ele SFs for " << year << " is not supported!";
       abort();
       }
-      
+
       SF = RecoSF*SelSF;
    }
-   
+
    //Muon SF
    if(abs(flav) == 13 )
    {
@@ -229,10 +229,10 @@ float LeptonSFHelper::getSF(int year, int flav, float pt, float eta, float SCeta
          edm::LogError("LeptonSFHelper::") << "Ele SFs for " << year << " is not supported!";
          abort();
       }
-      
+
       SF = SelSF;
    }
-   
+
     return SF;
 }
 
@@ -240,11 +240,11 @@ float LeptonSFHelper::getSFError(int year, int flav, float pt, float eta, float 
 {
    float RecoSF = 1.0;
    float SelSF = 1.0;
-   
+
    float RecoSF_Unc = 0.0;
    float SelSF_Unc = 0.0;
    float SFError = 0.0;
-   
+
    if(abs(flav) == 11) {
       if(year == 2016)
       {
@@ -289,8 +289,8 @@ float LeptonSFHelper::getSFError(int year, int flav, float pt, float eta, float 
          edm::LogError("LeptonSFHelper::") << "Ele SFs for " << year << " is not supported!";
          abort();
          }
-      
-      
+
+
       if(year == 2016)
       {
          if(isCrack)
@@ -303,9 +303,9 @@ float LeptonSFHelper::getSFError(int year, int flav, float pt, float eta, float 
             SelSF = h_Ele_notCracks_2016->GetBinContent(h_Ele_notCracks_2016->FindFixBin(SCeta, std::min(pt,199.f)));
             SelSF_Unc = h_Ele_notCracks_2016->GetBinError(h_Ele_notCracks_2016->FindFixBin(SCeta, std::min(pt,199.f)));
          }
-         
+
       }
-      
+
       else if(year == 2017)
       {
          if(isCrack)
@@ -319,7 +319,7 @@ float LeptonSFHelper::getSFError(int year, int flav, float pt, float eta, float 
             SelSF_Unc = h_Ele_notCracks_2017->GetBinError(h_Ele_notCracks_2017->FindFixBin(SCeta, std::min(pt,499.f)));
          }
       }
-      
+
       else if(year == 2018)
       {
          if(isCrack)
@@ -333,16 +333,16 @@ float LeptonSFHelper::getSFError(int year, int flav, float pt, float eta, float 
             SelSF_Unc = h_Ele_notCracks_2018->GetBinError(h_Ele_notCracks_2018->FindFixBin(SCeta, std::min(pt,499.f)));
          }
       }
-      
-      
+
+
       else {
          edm::LogError("LeptonSFHelper::") << "Ele SFs for " << year << " is not supported!";
          abort();
       }
-      
+
       SFError = sqrt( RecoSF_Unc*RecoSF_Unc/(RecoSF*RecoSF) + SelSF_Unc*SelSF_Unc/(SelSF*SelSF) ); // assume full correlation between different electrons (and uncorrelated reco and sel uncertainties)
    }
-   
+
    //Muon SF
    if(abs(flav) == 13 )
    {
@@ -365,10 +365,9 @@ float LeptonSFHelper::getSFError(int year, int flav, float pt, float eta, float 
          edm::LogError("LeptonSFHelper::") << "Ele SFs for " << year << " is not supported!";
          abort();
       }
-      
+
       SFError = SelSF_Unc/SelSF; // assume full correlation between different muons (and uncorrelated reco and sel uncertainties)
    }
-   
+
    return SFError;
 }
-
