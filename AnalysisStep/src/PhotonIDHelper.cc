1ecf58f5fadd25a2372a896bdf2699abaa73c3d7
jdavis36 <jdavi231@jhu.edu>
2023-04-17 04:28:04 -0400

    Add Photon Uncertainties to CJLST Trees (#166)
    * Add Photon Uncertainties to CJLST Trees
    
    * Add Energy Corrections for Photons to Tree
    
    * Remove Fix Me from Photon ID
    
    * Remove METFilter Pass Branch
    
    * remove extraneous file
    
    * revert ZNTupleMaker.cc

--- AnalysisStep/src/PhotonIDHelper.cc
+++ AnalysisStep/src/PhotonIDHelper.cc
@@ -17,11 +17,31 @@ bool PhotonIDHelper::isCutBasedID_Loose(int year, const pat::Photon& photon){
    
    bool isID = false;
    
-   // [FIXME] Removing photon ID for the moment since we do not use it in the analysis. Be careful if you want to use it, because the code will run locally but if you try to submit the jobs on Condor they will fail becuase dumpPython() replaces "-" with "_". I have contacted EGamma to provide a fix for this so maybe check if a new version of photon ID that does not have this problem exists before using it.
+   if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
+   if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
+   if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
    
-   if (year == 2016) isID = false; //photon.photonID("cutBasedPhotonID-Fall17-94X-V2-loose");
-   if (year == 2017) isID = false; //photon.photonID("cutBasedPhotonID-Fall17-94X-V2-loose");
-   if (year == 2018) isID = false; //photon.photonID("cutBasedPhotonID-Fall17-94X-V2-loose");
+   return isID;
+}
+
+bool PhotonIDHelper::isCutBasedID_Medium(int year, const pat::Photon& photon){
+   
+   bool isID = false;
+      
+   if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
+   if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
+   if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
+   
+   return isID;
+}
+
+bool PhotonIDHelper::isCutBasedID_Tight(int year, const pat::Photon& photon){
+   
+   bool isID = false;
+      
+   if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
+   if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
+   if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
    
    return isID;
 }
