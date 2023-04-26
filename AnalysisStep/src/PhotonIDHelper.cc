2dc87fd737f3b56382f1e2e521ea9a57f6a2e26d
Nicola Amapane <nicola.amapane@cern.ch>
2023-04-26 08:54:21 +0200

    Improve batch scripts and handling of transfer option

--- AnalysisStep/src/PhotonIDHelper.cc
+++ AnalysisStep/src/PhotonIDHelper.cc
@@ -17,9 +17,13 @@ bool PhotonIDHelper::isCutBasedID_Loose(int year, const pat::Photon& photon){
    
    bool isID = false;
    
-   if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
-   if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
-   if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
+   try { // dumpPython replaces "-" with "_", so we may find either depending on whether we run with batch scripts or locally.
+     if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
+     if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
+     if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
+   } catch (...) {
+     isID = photon.photonID("cutBasedPhotonID-Fall17-94X-V2-loose");
+   }
    
    return isID;
 }
@@ -27,21 +31,27 @@ bool PhotonIDHelper::isCutBasedID_Loose(int year, const pat::Photon& photon){
 bool PhotonIDHelper::isCutBasedID_Medium(int year, const pat::Photon& photon){
    
    bool isID = false;
-      
-   if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
-   if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
-   if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
-   
+   try {
+     if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
+     if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
+     if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
+   } catch (...) {
+      isID = photon.photonID("cutBasedPhotonID-Fall17-94X-V2-medium");
+   }
+
    return isID;
 }
 
 bool PhotonIDHelper::isCutBasedID_Tight(int year, const pat::Photon& photon){
    
    bool isID = false;
-      
-   if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
-   if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
-   if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
+   try {      
+     if (year == 2016) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
+     if (year == 2017) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
+     if (year == 2018) isID = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
+   } catch (...) {
+      isID = photon.photonID("cutBasedPhotonID-Fall17-94X-V2-tight");
+   }
    
    return isID;
 }
