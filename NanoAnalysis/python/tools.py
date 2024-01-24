02d116c45c56da8be40c8e4d9f7119f10dcc0fe5
Matteo Bonanomi <matteo9496@gmail.com>
2024-01-24 11:24:02 +0100

    [tools.py] generalize function to get sum of weights (#223)
    * [tools.py] generalize function to get sum of weights
    
    * [tools.py] pass TFile to get_genEventSumw. Make maxEntriesPerSample optional

--- NanoAnalysis/python/tools.py
+++ NanoAnalysis/python/tools.py
@@ -78,3 +78,35 @@ def lhe_logger(genpart):
     LHEPart = Collection (event, "LHEPart")
     for i, Lp in enumerate(LHEPart):
         print(i, Lp.pdgId, Lp.pt, Lp.eta, Lp.status, Lp.incomingpz)
+
+def get_genEventSumw(input_file, maxEntriesPerSample=None):
+    '''
+       Util function to get the sum of weights per event.
+       Returns the sum of weights, similarly to what we
+       stored in Counters->GetBinContent(40) in the miniAODs.
+    '''
+    f = input_file
+
+    runs  = f.Runs
+    event = f.Events
+    nRuns = runs.GetEntries()
+    nEntries = event.GetEntries()
+
+    iRun = 0
+    genEventCount = 0
+    genEventSumw = 0.
+
+    while iRun < nRuns and runs.GetEntry(iRun) :
+        genEventCount += runs.genEventCount
+        genEventSumw += runs.genEventSumw
+        iRun +=1
+    print ("gen=", genEventCount, "sumw=", genEventSumw)
+
+    if maxEntriesPerSample is not None:
+        print(f"Scaling to {maxEntriesPerSample} entries")
+        if nEntries>maxEntriesPerSample :
+            genEventSumw = genEventSumw*maxEntriesPerSample/nEntries
+            nEntries=maxEntriesPerSample
+        print("    scaled to:", nEntries, "sumw=", genEventSumw)
+
+    return genEventSumw
