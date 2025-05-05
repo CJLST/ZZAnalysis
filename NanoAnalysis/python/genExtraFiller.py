e2a1ca1c37fb22a80428df35e10ceb812d54da74
Nicholas <npinto7@jh.edu>
2025-05-05 15:16:08 +0200

    Added computation of Gen-Angles to genExtraFiller. NOTICE: Something in this implementation is wrong. Many angle calculations return NaN.

--- NanoAnalysis/python/genExtraFiller.py
+++ NanoAnalysis/python/genExtraFiller.py
@@ -25,11 +25,11 @@ class genExtraFiller(Module):
     def bookExtra(self, collName) :
         theLenVar="n"+collName
         # Book MELA angle branches
-        self.out.branch(collName + "_costheta1", "F", lenVar=theLenVar, limitedPrecision=12)
-        self.out.branch(collName + "_costheta2", "F", lenVar=theLenVar, limitedPrecision=12)
-        self.out.branch(collName + "_Phi", "F", lenVar=theLenVar, limitedPrecision=12)
-        self.out.branch(collName + "_costhetastar", "F", lenVar=theLenVar, limitedPrecision=12)
-        self.out.branch(collName + "_Phi1", "F", lenVar=theLenVar, limitedPrecision=12)
+        self.out.branch("GenZZ" + "_costheta1", "F",  limitedPrecision=12)
+        self.out.branch("GenZZ" + "_costheta2", "F",  limitedPrecision=12)
+        self.out.branch("GenZZ" + "_Phi", "F", limitedPrecision=12)
+        self.out.branch("GenZZ" + "_costhetastar", "F", limitedPrecision=12)
+        self.out.branch("GenZZ" + "_Phi1", "F",  limitedPrecision=12)
 
     def analyze(self, event) :
         self.addExtra('GenPart', event)
@@ -39,11 +39,12 @@ class genExtraFiller(Module):
 
     def addExtra(self, collName, event) :
         genpart = Collection(event, collName)
+        print("Z1l1idx: ", event.GenZZ_Z1l1Idx)
        
     
-        # genLeps = [genpart[event.GenZZ_Z1l1Idx], genpart[event.GenZZ_Z1l2Idx], genpart[event.GenZZ_Z2l1Idx], genpart[event.GenZZ_Z2l2Idx]]
+        genLeps = [genpart[event.GenZZ_Z1l1Idx], genpart[event.GenZZ_Z1l2Idx], genpart[event.GenZZ_Z2l1Idx], genpart[event.GenZZ_Z2l2Idx]]
 
-        genLeps = [genpart[GenZZ_Z1l1Idx], genpart[GenZZ_Z1l2Idx], genpart[GenZZ_Z2l1Idx], genpart[GenZZ_Z2l2Idx]]
+ 
         # MELA angle arrays
         helcosthetaZ1s = [-999.] * len(genpart)
         helcosthetaZ2s = [-999.] * len(genpart)
@@ -57,13 +58,15 @@ class genExtraFiller(Module):
         if self.MELA != None: 
 
             daughters = Mela.SimpleParticleCollection_t()
-            daughters.add_particle(Mela.SimpleParticle_t(genLeps[0].pdgId, genLeps[0].pt, genLeps[0].eta, genLeps[0].phi, genLeps[0].mass))
+            daughters.add_particle(Mela.SimpleParticle_t(genLeps[0].pdgId, genLeps[0].pt, genLeps[0].eta, genLeps[0].phi, getMass(genLeps[0].pdgId)))
+            # print("daughter 1: ", [genLeps[0].pdgId, genLeps[0].pt, genLeps[0].eta, genLeps[0].phi, getMass(genLeps[0].pdgId)])
             
-            daughters.add_particle(Mela.SimpleParticle_t(genLeps[1].pdgId, genLeps[1].pt, genLeps[1].eta, genLeps[1].phi, genLeps[1].mass))
-
-            daughters.add_particle(Mela.SimpleParticle_t(genLeps[2].pdgId, genLeps[2].pt, genLeps[2].eta, genLeps[2].phi, genLeps[2].mass))
-
-            daughters.add_particle(Mela.SimpleParticle_t(genLeps[3].pdgId, genLeps[3].pt, genLeps[3].eta, genLeps[3].phi, genLeps[3].mass))
+            daughters.add_particle(Mela.SimpleParticle_t(genLeps[1].pdgId, genLeps[1].pt, genLeps[1].eta, genLeps[1].phi, getMass(genLeps[1].pdgId)))
+            # print("daughter 2: ", [genLeps[1].pdgId, genLeps[1].pt, genLeps[1].eta, genLeps[1].phi, getMass(genLeps[1].pdgId)])
+            daughters.add_particle(Mela.SimpleParticle_t(genLeps[2].pdgId, genLeps[2].pt, genLeps[2].eta, genLeps[2].phi, getMass(genLeps[2].pdgId)))
+            # print("daughter 3: ", [genLeps[2].pdgId, genLeps[2].pt, genLeps[2].eta, genLeps[2].phi, getMass(genLeps[2].pdgId)])
+            daughters.add_particle(Mela.SimpleParticle_t(genLeps[3].pdgId, genLeps[3].pt, genLeps[3].eta, genLeps[3].phi, getMass(genLeps[3].pdgId)))
+            # print("daughter 4: ", [genLeps[3].pdgId, genLeps[3].pt, genLeps[3].eta, genLeps[3].phi, getMass(genLeps[3].pdgId)])
             
 
             self.MELA.setInputEvent(daughters, None, None, 0)
@@ -82,12 +85,21 @@ class genExtraFiller(Module):
             mZ2s = mZ2  
 
         # Fill MELA angle branches
-        self.out.fillBranch(collName + "_costheta1", helcosthetaZ1s)
-        self.out.fillBranch(collName + "_costheta2", helcosthetaZ2s)
-        self.out.fillBranch(collName + "_Phi", helPhis)
-        self.out.fillBranch(collName + "_costhetastar", costhetastars)
-        self.out.fillBranch(collName + "_Phi1", phistarZ1s)
+        self.out.fillBranch("GenZZ_costheta1", helcosthetaZ1s)
+        self.out.fillBranch("GenZZ_costheta2", helcosthetaZ2s)
+        self.out.fillBranch("GenZZ_Phi", helPhis)
+        self.out.fillBranch("GenZZ_costhetastar", costhetastars)
+        self.out.fillBranch("GenZZ_Phi1", phistarZ1s)
 
+
+def getMass(pdgId):
+    if abs(pdgId) == 11:
+        return 0.000511
+    elif abs(pdgId) == 13:
+        return 0.1057
+    elif abs(pdgId) == 15:
+        return 1.777
+    
 
 
         
