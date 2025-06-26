5ce59736c980535fd897d32a6449d3fe7d46ea38
Nicholas <npinto7@jh.edu>
2025-06-26 16:53:23 +0200

    Corrected the exampleProbabilities file.

--- NanoAnalysis/test/prod/pyFragments/exampleProbabilities.py
+++ NanoAnalysis/test/prod/pyFragments/exampleProbabilities.py
@@ -11,14 +11,14 @@
                           }, 
                           append=True)
 
-    setConf("probabilities", {'Name': "P_Gen_ggH_ghg2_1_ghz4_1", 
-                            "Process": "SelfDefine_spin0", 
-                            "MatrixElement":"JHUGen",
-                            "Production": "ZZGG", 
-                            "Couplings": {'ghz4':[1,0], 
-                                            'ghg2':[1,0]}, 
-                            "Prod": False,
-                            "Dec": True, 
-                            "isgen": True,
-                            "dividep": "Native",
-                            "computeprop": False }, append=True)
+setConf("probabilities", {'Name': "P_Gen_ggH_ghg2_1_ghz4_1", 
+                        "Process": "SelfDefine_spin0", 
+                        "MatrixElement":"JHUGen",
+                        "Production": "ZZGG", 
+                        "Couplings": {'ghz4':[1,0], 
+                                        'ghg2':[1,0]}, 
+                        "Prod": False,
+                        "Dec": True, 
+                        "isgen": True,
+                        "dividep": "Native",
+                        "computeprop": False }, append=True)
