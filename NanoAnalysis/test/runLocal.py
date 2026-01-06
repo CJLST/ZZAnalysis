b929af31d67a93eba7182872f0b903488f515a78
Nicola Amapane <nicola.amapane@cern.ch>
2026-01-06 13:47:46 +0100

    restore

--- NanoAnalysis/test/runLocal.py
+++ NanoAnalysis/test/runLocal.py
@@ -221,9 +221,10 @@ elif SampleToRun == "MELA_Test" :
     setConf("NANOVERSION", 15)
     setConf("store","root://cms-xrd-global.cern.ch/")
     # Add probabilities 
-    import prod.pyFragments.LHE_Probs_ggH0PM_M125
-    # import prod.pyFragments.EFT_RecoProbst
+    import prod.pyFragments.EFT_RecoProbs
+    # import prod.pyFragments.contextTest
     import prod.pyFragments.DefaultProbs
+    import prod.pyFragments.LHE_Probs_ggH0PM_M125
     setConf("fileNames", [
         "/store/mc/RunIII2024Summer24NanoAODv15/GluGluH-Hto2Zto4L_Par-M-125_TuneCP5_13p6TeV_powheg-jhugen-pythia8/NANOAODSIM/150X_mcRun3_2024_realistic_v2-v2/110000/a9e03ff9-2146-4aff-bd26-69abcb98359f.root" # 10000 evts
     ])
