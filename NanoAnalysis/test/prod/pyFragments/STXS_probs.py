d04e4e2033b71614d3a5f5e1cec86e66330803ac
Alessandro Tarabini <alessandro.tarabini@gmail.com>
2026-02-11 18:33:50 +0100

    add STXS pyFragment

--- NanoAnalysis/test/prod/pyFragments/STXS_probs.py
+++ NanoAnalysis/test/prod/pyFragments/STXS_probs.py
@@ -19,12 +19,12 @@ setConf("probabilities", {
     "Process": "HSMHiggs", 
     "Production": "JJVBF", 
     "MatrixElement": "JHUGen", 
-    "Couplings": {}, # TBC 
-    "Prod": True, # TBC (I set it to True, it is a production probability for me)
-    "Dec": False,  # TBC (I set it to False)
-    "ispm4l": False, # TBC (I set it to False from what it is written in the exampleProbabilities.py)
+    "Couplings": {},
+    "Prod": True, 
+    "Dec": False, 
+    "ispm4l": False, 
     "context": "Reco", 
-    "computeprop": False, # TBC
+    "computeprop": False,
     "useconstant": True, # This argument affects the normalization, and should generally be set to true for reconstructed events, false forgenerator level events. [from https://spin.pha.jhu.edu/Manual.pdf]
     "addPAux": True
     }, append=True)
@@ -35,10 +35,10 @@ setConf("probabilities", {
     "Process": "HSMHiggs", 
     "Production": "JJVBF", 
     "MatrixElement": "JHUGen", 
-    "Couplings": {}, # TBC 
-    "Prod": True, # TBC (I set it to True, it is a production probability for me)
-    "Dec": False,  # TBC (I set it to False)
-    "ispm4l": False, # TBC (I set it to False from what it is written in the exampleProbabilities.py)
+    "Couplings": {}, 
+    "Prod": True, 
+    "Dec": False,
+    "ispm4l": False,
     "context": "Reco", 
     "computeprop": False, # TBC
     "useconstant": True # This argument affects the normalization, and should generally be set to true for reconstructed events, false forgenerator level events. [from https://spin.pha.jhu.edu/Manual.pdf]
@@ -50,10 +50,10 @@ setConf("probabilities", {
     "Process": "HSMHiggs", 
     "Production": "JQCD", 
     "MatrixElement": "JHUGen", 
-    "Couplings": {}, # TBC 
-    "Prod": True, # TBC (I set it to True, it is a production probability for me)
-    "Dec": False,  # TBC (I set it to False)
-    "ispm4l": False, # TBC (I set it to False from what it is written in the exampleProbabilities.py)
+    "Couplings": {},
+    "Prod": True,
+    "Dec": False,  
+    "ispm4l": False, 
     "context": "Reco", 
     "computeprop": False, # TBC
     "useconstant": True # This argument affects the normalization, and should generally be set to true for reconstructed events, false forgenerator level events. [from https://spin.pha.jhu.edu/Manual.pdf]
@@ -65,10 +65,10 @@ setConf("probabilities", {
     "Process": "HSMHiggs", 
     "Production": "Had_WH", 
     "MatrixElement": "JHUGen", 
-    "Couplings": {}, # TBC 
-    "Prod": True, # TBC (I set it to True, it is a production probability for me)
-    "Dec": False,  # TBC (I set it to False)
-    "ispm4l": False, # TBC (I set it to False from what it is written in the exampleProbabilities.py)
+    "Couplings": {},
+    "Prod": True,
+    "Dec": False, 
+    "ispm4l": False,
     "context": "Reco", 
     "computeprop": False, # TBC
     "useconstant": True, # This argument affects the normalization, and should generally be set to true for reconstructed events, false forgenerator level events. [from https://spin.pha.jhu.edu/Manual.pdf]
@@ -82,12 +82,12 @@ setConf("probabilities", {
     "Process": "HSMHiggs", 
     "Production": "Had_ZH", 
     "MatrixElement": "JHUGen", 
-    "Couplings": {}, # TBC 
-    "Prod": True, # TBC (I set it to True, it is a production probability for me)
-    "Dec": False,  # TBC (I set it to False)
-    "ispm4l": False, # TBC (I set it to False from what it is written in the exampleProbabilities.py)
+    "Couplings": {},  
+    "Prod": True,
+    "Dec": False, 
+    "ispm4l": False,
     "context": "Reco", 
-    "computeprop": False, # TBC
+    "computeprop": False, 
     "useconstant": True, # This argument affects the normalization, and should generally be set to true for reconstructed events, false forgenerator level events. [from https://spin.pha.jhu.edu/Manual.pdf]
     "addPmavjj": True,
     "addPmavjj_true": True
@@ -99,11 +99,11 @@ setConf("probabilities", {
     "Process": "HSMHiggs", 
     "Production": "JJQCD", 
     "MatrixElement": "JHUGen", 
-    "Couplings": {}, # TBC 
-    "Prod": True, # TBC (I set it to True, it is a production probability for me)
-    "Dec": False,  # TBC (I set it to False)
-    "ispm4l": False, # TBC (I set it to False from what it is written in the exampleProbabilities.py)
+    "Couplings": {}, 
+    "Prod": True, 
+    "Dec": False, 
+    "ispm4l": False,
     "context": "Reco", 
-    "computeprop": False, # TBC
+    "computeprop": False,
     "useconstant": True # This argument affects the normalization, and should generally be set to true for reconstructed events, false forgenerator level events. [from https://spin.pha.jhu.edu/Manual.pdf]
     }, append=True)
