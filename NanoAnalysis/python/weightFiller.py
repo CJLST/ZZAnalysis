19de18af86641a0f513872e049eca7fd18cbb6d4
Mohit Srivastav <vir.moh@gmail.com>
2026-09-08 16:58:37 -0400

    full info

--- NanoAnalysis/python/weightFiller.py
+++ NanoAnalysis/python/weightFiller.py
@@ -14,8 +14,10 @@ class weightFiller(Module):
     def __init__(self, XS, APPLY_K_NNLOQCD_ZZGG, APPLY_K_NNLOQCD_NLOEW_ZZQQB, APPLY_QCD_GGF_UNCERT, LEPTON_SETUP):
         print(
             "***weightFiller: XS:", XS, 
+            "APPLY_K_NNLOQCD_ZZGG:", APPLY_K_NNLOQCD_ZZGG,
             "APPLY_K_NNLOQCD_NLOEW_ZZQQB:", APPLY_K_NNLOQCD_NLOEW_ZZQQB, 
             "APPLY_QCD_GGF_UNCERT:", APPLY_QCD_GGF_UNCERT, 
+            "LEPTON_SETUP:", LEPTON_SETUP
             flush=True
         )
         self.writeHistFile = False
